48edc28b2f91daaa528ff3cea8efcd89df6459f1
Mike Hughes <mike@michaelchughes.com>
2013-11-11 22:01:01 -0500

    ENH halfway thru major reorg

---
+++ bnpy/ioutil/BNPYArgParser.py
@@ -0,0 +1,161 @@
+import argparse
+import ConfigParser
+import os
+
+OnlineDataAlgSet = ['soVB', 'moVB']
+
+# Hard-code the relative path to config/
+cfgroot = os.path.sep.join(os.path.abspath(__file__).split(os.path.sep)[:-2])
+cfgroot = os.path.join(cfgroot, 'config/')
+ConfigPaths={cfgroot + 'allocmodel.conf':'allocModelName',
+             cfgroot + 'obsmodel.conf':'obsModelName', 
+             cfgroot + 'inference.conf':'algName',
+             cfgroot + 'init.conf':None,
+             cfgroot + 'output.conf':None}             
+
+OnlineDataConfigPath =  cfgroot + 'onlinedata.conf'
+
+
+UNKARGLIST = None
+
+def parseUnknownArgs():
+  return arglist_to_kwargs(UNKARGLIST)
+
+def parseRequiredArgs():
+  parser = argparse.ArgumentParser()
+  parser.add_argument('dataName')
+  parser.add_argument('allocModelName')
+  parser.add_argument('obsModelName')
+  parser.add_argument('algName')
+  args, unk = parser.parse_known_args()
+  return args.__dict__
+  
+def parseKeywordArgs(ReqArgs, **kwargs):
+  global UNKARGLIST
+  if ReqArgs['algName'] in OnlineDataAlgSet:
+    ConfigPaths[OnlineDataConfigPath] = None
+  else:
+    if OnlineDataConfigPath in ConfigPaths:
+      del ConfigPaths[OnlineDataConfigPath]
+
+  # BUILD parser using default opts in the config files
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--moves', type=str)
+    
+  for fpath, secName in ConfigPaths.items():
+    if secName is not None:
+      secName = ReqArgs[secName]
+    addArgGroupFromConfigFile(parser, fpath, secName) 
+    if fpath.count('infer') > 0:
+      addArgGroupFromConfigFile(parser, fpath, 'birth') 
+      addArgGroupFromConfigFile(parser, fpath, 'merge') 
+
+  # PARSE keyword args
+  if len(kwargs.keys()) > 0:
+    alist = kwargs_to_arglist(**kwargs)
+    args, UNKARGLIST = parser.parse_known_args(alist)
+  else:
+    args, UNKARGLIST = parser.parse_known_args()
+  if args.moves is not None:
+    args.moves = args.moves.split(',')
+  
+  # CONVERT parsed arg namespace to a nice dictionary
+  argDict = dict()
+  for fpath, secName in ConfigPaths.items():
+    if secName is not None:
+      secName = ReqArgs[secName]
+    addArgsToDictByConfigFile(argDict, args, fpath, secName)
+    if fpath.count('infer') > 0 and args.moves is not None:
+      for moveName in args.moves:
+        addArgsToDictByConfigFile(argDict, args, fpath, moveName)
+  return argDict
+
+def arglist_to_kwargs(alist):
+  kwargs = dict()
+  a = 0
+  while a < len(alist):
+    curarg = alist[a]
+    if curarg.startswith('--'):
+      argname = curarg[2:]
+      argval = alist[a+1]
+      curType = getType(argval)
+      kwargs[argname] = curType(argval)
+      a += 1
+    a += 1
+  return kwargs
+
+def kwargs_to_arglist(**kwargs):
+  arglist = list()
+  for key,val in kwargs.items():
+    arglist.append('--' + key)
+    arglist.append(str(val))
+  return arglist
+
+def addArgsToDictByConfigFile(argDict, args, filepath, targetSectionName=None):
+  config = readConfigParser( filepath)
+  for secName in config.sections():
+    if secName.count("Help") > 0:
+      continue
+    if targetSectionName is not None:
+      if secName != targetSectionName:
+        continue
+    DefDict = dict(config.items(secName))  
+    secArgDict = dict([ (k,v) for (k,v) in vars(args).items() if k in DefDict])
+    argDict[secName] = secArgDict
+
+def addArgGroupFromConfigFile(parser, confFilePath, targetSectionName=None):
+  config = readConfigParser(confFilePath)
+  for secName in config.sections():
+    if secName.count("Help") > 0:
+      continue
+    if targetSectionName is not None:
+      if secName != targetSectionName:
+        continue
+    DefDict = dict( config.items( secName ) )
+    try:
+      HelpDict = dict( config.items( secName+"Help") )
+    except ConfigParser.NoSectionError:
+      HelpDict = dict()
+      
+    group = parser.add_argument_group(secName)    
+    for optName, defVal in DefDict.items():
+      defType = getType( defVal)
+      if optName in HelpDict:
+        helpMsg = '[def=%s] %s' % (defVal, HelpDict[optName])
+      else:
+        helpMsg = '[def=%s]' % (defVal)
+      
+      if defType == True or defType == False:
+        group.add_argument( '--%s' % (optName), default=defType, help=helpMsg, action='store_true')
+      else:
+        group.add_argument( '--%s' % (optName), default=defVal, help=helpMsg, type=defType)
+
+
+def readConfigParser(filepath):
+  config = ConfigParser.SafeConfigParser()
+  config.optionxform = str
+  config.read( filepath )  
+  return config
+
+def getType( defVal ):
+  ''' Determine Python type from the provided default value
+      Returns
+      ---------
+      a Python type object
+      {True, False, int, float, str}
+  '''
+  if defVal == 'true' or defVal == 'True':
+    return True
+  if defVal == 'false' or defVal == 'False':
+    return False
+  try:
+    int(defVal)
+    return int
+  except Exception:
+    pass
+  try:
+    float(defVal)
+    return float
+  except Exception:
+    return str
+
---
+++ bnpy/viz/PlotELBO.py
@@ -0,0 +1,184 @@
+'''
+PlotELBO.py
+
+Basic executable for plotting the ELBO vs. time/iterations/num. data items processed
+
+Usage
+-------
+python PlotELBO.py dataName allocModelName obsModelName algNames[comma-separated] [options]
+
+Options
+--------
+--savefilename : absolute path to directory to save figure
+                 Ex: ~/Desktop/myfigure.pdf or ~/Desktop/myfigure.png
+                 
+--xvar : name of x axis variable to plot. one of {iters,laps,times}
+--taskids : ids of the tasks (individual runs) of the given job to plot.
+             Ex: "1" or "3" or "1,2,3" or "1-6"
+'''
+from matplotlib import pylab
+import numpy as np
+import argparse
+import glob
+import os
+
+Colors = [(0,0,0), # black
+          (0,0,1), # blue
+          (1,0,0), # red
+          (0,1,0.25), # green (darker)
+          (1,0,1), # magenta
+          (0,1,1), # cyan
+          (1,0.6,0), #orange
+         ]
+
+YMin = None
+YMax = None
+
+XLabelMap = dict(laps='num pass thru data',
+                  iters='num steps in alg',
+                  times='elapsed time (sec)'
+                  )
+                  
+def parse_task_ids(jobpath, taskids):
+  if taskids is None:
+    fulltaskpaths = glob.glob(os.path.join(jobpath,'*'))
+    taskids = [os.path.split(tpath)[-1] for tpath in fulltaskpaths]
+  elif taskids.count(',') > 0:
+    taskids = [t for t in taskids.split(',')]
+  elif taskids.count('-') > 0:
+    fields = taskids.split('-')
+    if not len(fields)==2:
+      raise ValueError("Bad taskids specification")
+    fields = np.int32(np.asarray(fields))
+    taskids = np.arange(fields[0],fields[1]+1)
+    taskids = [str(t) for t in taskids]
+  else:
+    taskids = taskids
+  return taskids
+             
+             
+def plot_all_tasks_for_job(jobpath, args, jobname=None, color=None):
+  ''' Create line plot in current matplotlib figure
+      for each task/run of the designated jobpath
+  '''
+  if not os.path.exists(jobpath):
+    raise ValueError("No such path: %s" % (jobpath))
+  
+  taskids = parse_task_ids(jobpath, args.taskids)
+    
+  xAll = list()
+  yAll = list()
+  xLocs = list()
+  yLocs = list()
+  for tt, taskid in enumerate(taskids):
+    xs = np.loadtxt(os.path.join(jobpath, taskid, args.xvar+'.txt'))
+    ys = np.loadtxt(os.path.join(jobpath, taskid, 'evidence.txt'))
+    if color is None:
+      pylab.plot(xs, ys, '.-', markersize=10, linewidth=2)
+    else:
+      # remove first-lap content for moVB
+      if jobpath.count('moVB') > 0 and args.xvar == 'laps':
+        mask = xs >= 1.0
+        xs = xs[mask]
+        ys = ys[mask]
+      if tt == 0:
+        label = jobname
+      else:
+        label = None
+      pylab.plot(xs, ys, '.-', markersize=10, linewidth=2, color=color, label=label)
+    if len(ys) > 0:
+      xLocs.append(xs[-1])
+      yLocs.append(ys[-1])
+      yAll.extend(ys[1:])
+      xAll.extend(xs[1:])
+      
+  # Zoom in to the useful part of the ELBO trace
+  if len(yAll) > 0:
+    global YMin, YMax
+    ymin = np.percentile(yAll, 1)
+    ymax = np.max(yAll)
+    if YMin is None:
+      YMin = ymin
+      YMax = ymax
+    else:
+      YMin = np.minimum(ymin, YMin)
+      YMax = np.maximum(YMax, ymax)
+    blankmargin = 0.08*(YMax - YMin)
+    pylab.ylim( [YMin, YMax + blankmargin])
+    
+  if args.doShowTaskNums and len(taskids) > 0:
+    yNudge = 0.03 * (ymax - ymin)
+    for tt in range(len(taskids)):
+      xNudge = 0.05 * (np.max(xAll) - np.min(xAll))
+      xNudge += 0.25*np.random.randn() * xNudge
+      pylab.text( xLocs[tt] - xNudge, yLocs[tt] - yNudge, str(taskids[tt]))
+  pylab.xlabel(XLabelMap[args.xvar])
+  pylab.ylabel('log evidence')
+  
+                  
+def main():
+  parser = argparse.ArgumentParser()
+  parser.add_argument('dataName', type=str, \
+        help='name of python script that produces data to analyze.')
+  parser.add_argument('allocModelName', type=str, \
+        help='name of allocation model. {MixModel, DPMixModel}')
+  parser.add_argument('obsModelName', type=str, \
+        help='name of observation model. {Gauss, ZMGauss}')
+  parser.add_argument('algNames', type=str, \
+        help='names of learning algorithms to consider, {EM, VB, moVB, soVB}. comma separated if multiple.')
+  parser.add_argument('--jobnames', type=str, default='defaultjob',
+        help='name of experiment whose results should be plotted')
+  parser.add_argument('--legendnames', type=str, default=None,
+        help='comma sep list of names to show on legend for each job')
+  parser.add_argument('--taskids', type=str, default=None,
+        help="int ids of the tasks (individual runs) of the given job to plot." +\
+              'Ex: "1" or "3" or "1,2,3" or "1-6"')
+  parser.add_argument('--xvar', type=str, default='laps',
+        help="name of x axis variable to plot. one of {iters,laps,times}")
+  parser.add_argument('--savefilename', type=str, default=None,
+        help="absolute path to directory to save figure")
+  parser.add_argument('--doShowTaskNums', action='store_true', default=False,
+        help="if present, do show task numbers next to corresponding line plot")
+  args = parser.parse_args()
+
+  rootpath = os.path.join(os.environ['BNPYOUTDIR'], args.dataName, \
+                              args.allocModelName, args.obsModelName)
+  algnames = args.algNames.split(',')
+  jobnames = args.jobnames.split(',')
+  if args.legendnames is not None:
+    legnames = args.legendnames.split(',')
+    assert len(legnames) == len(jobnames)
+  cID = 0
+  for algname in algnames:
+    for jobname in jobnames:
+      curjobpath = os.path.join(rootpath, algname, jobname)
+      if not os.path.exists(curjobpath):
+        continue
+      cID += 1
+      if len(algnames) > 1:
+        color = Colors[cID]
+        if len(jobnames) > 1:
+          curjobname = algname + ' ' + jobname
+        else:
+          curjobname = algname
+      elif len(jobnames) > 1:
+        color = Colors[cID]
+        curjobname = jobname
+      else:
+        color = None
+        curjobname = None
+      if args.legendnames is not None:
+        curjobname = legnames[cID - 1]
+      plot_all_tasks_for_job(curjobpath, args, jobname=curjobname, color=color)    
+  if cID > 1:
+    pylab.legend(loc='best')
+  
+  if args.savefilename is not None:
+    pylab.show(block=False)
+    pylab.savefig(args.savefilename)
+  else:
+    pylab.show(block=True)
+  
+if __name__ == "__main__":
+  main()
+
