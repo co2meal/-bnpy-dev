e451e2b4375e4390c1b3610b9caa09f8a57e79be
Mike Hughes <mike@michaelchughes.com>
2013-12-01 13:43:32 -0700

    FIX removed unneeded comments

--- bnpy/distr/__init__.py
+++ bnpy/distr/__init__.py
@@ -5,7 +5,5 @@ from ZMGaussDistr import ZMGaussDistr
 from WishartDistr import WishartDistr
 from GaussWishDistr import GaussWishDistr
 from DirichletDistr import DirichletDistr
-#from MultinomialDistr import MultinomialDistr
 
-#__all__ = ['WishartDistr', 'ZMGaussDistr', 'GaussDistr', 'GaussWishDistr','DirichletDistr','MultinomialDistr']
 __all__ = ['WishartDistr', 'ZMGaussDistr', 'GaussDistr', 'GaussWishDistr','DirichletDistr']
