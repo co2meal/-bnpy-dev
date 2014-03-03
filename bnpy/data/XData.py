1a9f4479b3a1652e7dabbe8647a4cd665221e6ec
Mike Hughes <mhughes@cs.brown.edu>
2014-03-03 15:07:51 -0500

    ENH added Hashtag dataset for DiagGauss exploration

--- bnpy/data/XData.py
+++ bnpy/data/XData.py
@@ -1,12 +1,23 @@
 '''
 XData.py
 
-Data object for holding a dense matrix X of real numbers,
-where each row of X is a single observation.
+Data object for holding a dense matrix X of real 64-bit floating point numbers,
+Each row of X represents a single observation.
 
-This object guarantees underlying numpy array representation is best for math ops.
-This means byteorder and C-contiguity are standardized.
+Example
+--------
+>> import numpy as np
+>> from bnpy.data import XData
+>> X = np.random.randn(1000, 3) # Create 1000x3 matrix
+>> myData = XData(X)
+>> print myData.nObs
+1000
+>> print myData.D
+3
+>> print myData.X.shape
+(1000,3)
 '''
+
 import numpy as np
 from .DataObj import DataObj
 from .MinibatchIterator import MinibatchIterator
@@ -24,8 +35,9 @@ class XData(DataObj):
     return cls( InDict['X'], nObsTotal )
   
   def __init__(self, X, nObsTotal=None, TrueZ=None):
-    ''' Constructor for building an instance of XData given an array
-        Ensures array is 2-dimensional with proper byteorder, contiguity, and ownership
+    ''' Create an instance of XData given an array
+        Reallocation of memory may occur, to ensure that 
+          X is a 2D numpy array with proper byteorder, contiguity, and ownership.
     '''
     X = np.asarray(X)
     if X.ndim < 2:
