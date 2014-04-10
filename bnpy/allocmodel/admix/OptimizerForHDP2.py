695755ca8699b371c308d1bd1208aa6e174e19d7
Mike Hughes <mike@michaelchughes.com>
2014-04-10 09:23:56 -0400

    ENH OptimizerForHDP2, which reparameterizes objective to make learning easier, now works and has some solid unit tests.

--- bnpy/allocmodel/admix/OptimizerForHDP2.py
+++ bnpy/allocmodel/admix/OptimizerForHDP2.py
@@ -86,46 +86,53 @@ def find_optimum_multiple_tries(
   Info['nOverflow'] = nOverflow
   return eta, feta, Info      
 
-def find_optimum(sumLogPi=None, nDoc=0, gamma=1.0, alpha=1.0, initeta=None, approx_grad=False, factr=1.0e7, **kwargs):
+def find_optimum(
+          sumLogPi=None, nDoc=0, gamma=1.0, alpha=1.0,
+          initrho=None, initomega=None,
+          approx_grad=False, factr=1.0e7, **kwargs):
   ''' Run gradient optimization to estimate best vectors
         rho, omega for specified problem
 
       Returns
       --------
-      rho : 1D array, length K
-      omega : 1D array, length K
+      rhoomega : 1D array, length 2*K
       f : scalar value of minimization objective
       Info : dict
 
-      Raises
-      --------
-      ValueError with FAILURE in message if all restarts fail
       Raises
       --------
       ValueError on an overflow, any NaN, or failure to converge
   '''
   sumLogPi = np.squeeze(np.asarray(sumLogPi, dtype=np.float64))
   assert sumLogPi.ndim == 1
-  K = sumLogPi.size 
-  if initeta is None:
-    if nDoc == 0:
-      initeta = 0.5 * gamma.copy()
-    elif len(gamma) == 1:
-      initeta = gamma * np.ones(K)
-    else:
-      initeta = gamma.copy()
-  assert initeta.size == K
+  K = sumLogPi.size - 1
 
-  initc = np.log(initeta)
-  myFunc = lambda c: objFunc_c(c, sumLogPi=sumLogPi, nDoc=nDoc,
-                                   gamma=gamma, alpha=alpha,
-                                   approx_grad=approx_grad)
+  ## Determine initial value
+  if initrho is None:
+    initrho = create_initrho(K)
+  if initomega is None:
+    initomega = (nDoc + 1) * np.ones(K)
+  assert initrho.size == K
+  assert initomega.size == K
+  assert initrho.min() > 0.0
+  assert initrho.max() < 1.0
+  assert initomega.min() > 0.0
+  initrhoomega = np.hstack([initrho, initomega])
+
+  initc = rhoomega2c(initrhoomega)
+
+  ## Define objective function (unconstrained!)
+  objFunc = lambda c: objFunc_unconstrained(c, 
+                              sumLogPi=sumLogPi, nDoc=nDoc, 
+                              gamma=gamma, alpha=alpha,
+                              approx_grad=approx_grad)
   
+  ## Run optimization and catch any overflow or NaN issues
   with warnings.catch_warnings():
     warnings.filterwarnings('error', category=RuntimeWarning,
                                message='overflow')
     try:
-      chat, fhat, Info = scipy.optimize.fmin_l_bfgs_b(myFunc, initc,
+      chat, fhat, Info = scipy.optimize.fmin_l_bfgs_b(objFunc, initc,
                                                   disp=None,
                                                   approx_grad=approx_grad,
                                                   factr=factr,
@@ -138,30 +145,43 @@ def find_optimum(sumLogPi=None, nDoc=0, gamma=1.0, alpha=1.0, initeta=None, appr
   if Info['warnflag'] > 1:
     raise ValueError("FAILURE: " + Info['task'])
 
-  etahat = np.exp(chat)
-  Info['init'] = initeta
-  Info['objFunc'] = lambda eta: objFunc_eta(eta, sumLogPi=sumLogPi, nDoc=nDoc,
-                                   gamma=gamma, alpha=alpha,
-                                   approx_grad=approx_grad)
-  
-  return etahat, fhat, Info
+  Info['init'] = initrhoomega
+  Info['objFunc'] = lambda rhoomega: objFunc_constrained(rhoomega,
+                              sumLogPi=sumLogPi, nDoc=nDoc, 
+                              gamma=gamma, alpha=alpha,
+                              approx_grad=False)
+  rhoomega = c2rhoomega(chat, doGrad=False)
+  return rhoomega, fhat, Info
+
+def create_initrho(K):
+  REM = 1.0/(K*K*K)
+  beta = np.ones(K+1)
+  beta[:-1] = (1.0-REM)/K
+  beta[-1] = REM  
+  return _beta2v(beta)
 
 ########################################################### Objective
 ###########################################################  unconstrained
 def objFunc_unconstrained(c, approx_grad=False, **kwargs):
-  rhoomega, drodc = c2rhoomega(c)
+  rhoomega, drodc = c2rhoomega(c, doGrad=True)
   f, grad = objFunc_constrained(rhoomega, **kwargs)
   if approx_grad:
     return f
   return f, grad * drodc
 
-def c2rhoomega(c):
-  K = c.size
+def c2rhoomega(c, doGrad=False):
+  K = c.size/2
   rho = sigmoid(c[:K])
   omega = np.exp(c[K:])
   rhoomega = np.hstack([rho, omega])
+  if not doGrad:
+    return rhoomega
   drodc = np.hstack([rho*(1-rho), omega])
   return rhoomega, drodc
+
+def rhoomega2c( rhoomega):
+  K = rhoomega.size/2
+  return np.hstack([invsigmoid(rhoomega[:K]), np.log(rhoomega[K:])])
 
 def sigmoid(c):
   ''' sigmoid(c) = 1./(1+exp(-c))
@@ -200,13 +220,17 @@ def objFunc_constrained(rhoomega,
   '''
   assert not np.any(np.isnan(rhoomega))
   rho, omega, K = _unpack(rhoomega)
-  Ebeta = _rho2beta(rho)
-  kvec = K + 1 - np.arange(1, K+1)
+  Ebeta = _v2beta(rho)
 
-  u1 = rho*omega
-  u0 = (1-rho)*omega
+  u1 = rho * omega
+  u0 = (1 - rho) * omega
+
+
+  logc = np.sum(gammaln(u1) + gammaln(u0) - gammaln(omega))
   if nDoc > 0:
-    B1 = 1 + (1 - u1)/nDoc
+    logc = logc/nDoc
+    B1 = 1 + (1.0 - u1)/nDoc
+    kvec = K + 1 - np.arange(1, K+1)
     C1 = kvec + (alpha - u0)/nDoc
   else:
     B1 = 1 - u1
@@ -216,7 +240,7 @@ def objFunc_constrained(rhoomega,
   B2 = digamma(u1) - digammaomega
   C2 = digamma(u0) - digammaomega
 
-  elbo = np.sum(gammaln(u1) + gammaln(u0) - gammaln(omega)) \
+  elbo = logc \
          + np.inner( B1, B2) \
          + np.inner( C1, C2)
   if nDoc > 0:
@@ -231,7 +255,7 @@ def objFunc_constrained(rhoomega,
 
   gradrho = B1 * omega * psiP_u1 - C1 * omega * psiP_u0
   gradomega = B1 * (   rho * psiP_u1 - psiP_omega) \
-                + C1 * ((1-rho)* psiP_u0 - psiP_omega)
+            + C1 * ((1-rho)* psiP_u0 - psiP_omega)
 
   if nDoc > 0:
     # BetaMat : K x K+1 matrix
@@ -247,24 +271,48 @@ def objFunc_constrained(rhoomega,
   return -1.0 * elbo, -1.0 * grad
 
 def _unpack(rhoomega):
-  K = rhoomega.size
+  K = rhoomega.size / 2
   rho = rhoomega[:K]
   omega = rhoomega[-K:]
   return rho, omega, K
 
-def _rho2beta(rho):
-  ''' Convert to stick-breaking fractions rho to probability vector beta
+def _v2beta(v):
+  ''' Convert to stick-breaking fractions v to probability vector beta
       Args
       --------
-      rho : K-len vector, rho[k] in interval [0, 1]
+      v : K-len vector, rho[k] in interval [0, 1]
       
       Returns
       --------
       beta : K+1-len vector, with positive entries that sum to 1
   '''
-  beta = np.hstack([1.0, np.cumprod(1-rho)])
-  beta[:-1] *= rho
+  beta = np.hstack([1.0, np.cumprod(1-v)])
+  beta[:-1] *= v
   return beta
+
+
+def _beta2v( beta ):
+  ''' Convert probability vector beta to stick-breaking fractions v
+      Args
+      --------
+      beta : K+1-len vector, with positive entries that sum to 1
+      
+      Returns
+      --------
+      v : K-len vector, v[k] in interval [0, 1]
+  '''
+  beta = np.asarray(beta)
+  K = beta.size
+  v = np.zeros(K-1)
+  cb = beta.copy()
+  for k in range(K-1):
+    cb[k] = 1 - cb[k]/np.prod( cb[:k] )
+    v[k] = beta[k]/np.prod( cb[:k] )
+  # Force away from edges 0 or 1 for numerical stability  
+  v = np.maximum(v,EPS)
+  v = np.minimum(v,1-EPS)
+  return v
+
 
 lowTriIDsDict = dict()
 def _get_lowTriIDs(K):
