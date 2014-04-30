aacd6cf3100265ef81972de2b0fbfb6f1334a329
Mike Hughes <mike@michaelchughes.com>
2014-04-30 08:53:17 -0400

    ENH updated TargetPlanner to sample underpredicted topics, and underpredicted words

--- bnpy/birthmove/TargetPlanner.py
+++ bnpy/birthmove/TargetPlanner.py
@@ -92,6 +92,115 @@ def select_target_comp(K, SS=None, model=None, LP=None, Data=None,
       print msg % (kk, ps[kk], lapsSinceLastBirth[kk], SS.N[kk])
 '''
 
+def calc_underprediction_scores_per_topic(K, model, Data, 
+                                                LP=None, **kwargs):
+  ''' Calculate for each topic a scalar weight. Larger => worse prediction.
+  '''
+  if LP is None:
+    LP = model.calc_local_params(Data)
+  assert K == model.allocModel.K
+  NdkThr = 1.0/K * kwargs['targetMinWordsPerDoc']
+  score = np.zeros(K)
+  for k in range(K):
+    lamvec = model.obsModel.comp[k].lamvec
+    modelWordFreq_k = lamvec / lamvec.sum()
+    candidateDocs = np.flatnonzero(LP['DocTopicCount'][:,k] > NdkThr)
+    for d in candidateDocs:
+      start = Data.doc_range[d,0]
+      stop = Data.doc_range[d,1]
+      word_id = Data.word_id[start:stop]
+      word_count = Data.word_count[start:stop]
+      resp = LP['word_variational'][start:stop, k]
+      empWordFreq_k = np.zeros(Data.vocab_size)
+      empWordFreq_k[word_id] = (resp * word_count)
+      empWordFreq_k = empWordFreq_k / empWordFreq_k.sum()
 
+      score[k] += LP['theta'][d,k] * calcKL_discrete(empWordFreq_k, 
+                                                     modelWordFreq_k)
+  return score
 
+def calc_underprediction_scores_per_word(K, model, Data, LP=None, **kwargs):
+  ''' Calculate for each vocab word a scalar score. Larger => worse prediction.
+  '''
+  if LP is None:
+    LP = model.calc_local_params(Data)
+  DocWordFreq_emp = calcWordFreqPerDoc_empirical(Data)
+  DocWordFreq_model = calcWordFreqPerDoc_model(model, LP)
+  uError = np.maximum( DocWordFreq_emp - DocWordFreq_model, 0)
+  # For each word, identify set of relevant documents
+  DocWordMat = Data.to_sparse_docword_matrix().toarray()
+  medianWordCount = np.median(DocWordMat, axis=0)
+  score = np.zeros(Data.vocab_size)
+  for vID in xrange(Data.vocab_size):
+    candidateDocs = np.flatnonzero(DocWordMat[:, vID] > medianWordCount[vID])
+    if len(candidateDocs) < 10:
+      continue
+    score[vID] = np.sum(uError[candidateDocs])
+  score = score - np.mean(score) # Center the scores!
+  score = np.maximum(score, 0)
+  score /= score.sum()
+  return score
+                  
 
+def sample_related_words_by_score(Data, pscore, nWords=3, anchor=None,
+                                                          doVerbose=False,     
+                                                          **kwargs):
+  ''' Sample set of words that have high underprediction score AND cooccur.
+  '''
+  DocWordArr = Data.to_sparse_docword_matrix().toarray()
+  Corr = np.corrcoef(DocWordArr.T, bias=1)
+  posCorr = np.maximum(Corr, 0)
+  randstate = kwargs['randstate']
+  if anchor is None:
+    anchors = randstate.choice(Data.vocab_size, nWords, replace=False, p=pscore)
+  else:
+    anchors = [anchor]
+  for firstWord in anchors:
+    curWords = [firstWord]
+    while len(curWords) < nWords:
+      relWordProbs = calc_prob_related_words(posCorr, pscore, curWords)
+      if np.sum(relWordProbs) < 1e-14:
+        break
+      newWord = randstate.choice(Data.vocab_size, 1, replace=False,
+                                                     p=relWordProbs)
+      curWords.append(int(newWord))
+      if doVerbose:
+        print curWords
+    if len(curWords) == nWords:
+      return curWords
+  return anchors
+
+def calc_prob_related_words(posCorr, pscore, curWords):
+  relWordProbs = np.prod(posCorr[curWords,:], axis=0) * pscore
+  relWordProbs[curWords] = 0
+  relWordProbs = np.maximum(relWordProbs, 0)
+  relWordProbs /= relWordProbs.sum()
+  return relWordProbs
+                         
+def calcWordFreqPerDoc_empirical(Data, candidateDocs=None):
+  ''' Build empirical distribution over words for each document.
+  '''
+  DocWordMat = Data.to_sparse_docword_matrix()
+  if candidateDocs is None:
+    DocWordFreq_emp = DocWordMat.toarray()
+  else:
+    DocWordFreq_emp = DocWordMat[candidateDocs].toarray()
+  DocWordFreq_emp /= 1e-9 + DocWordFreq_emp.sum(axis=1)[:,np.newaxis]
+  return DocWordFreq_emp
+
+def calcWordFreqPerDoc_model(model, LP, candidateDocs=None):
+  ''' Build model's expected word distribution for each document
+  '''
+  if candidateDocs is None:
+    Prior = np.exp( LP['E_logPi']) # D x K
+  else:
+    Prior = np.exp( LP['E_logPi'][candidateDocs]) # D x K
+  Lik = np.exp(model.obsModel.getElogphiMatrix()) # K x V
+  DocWordFreq_model = np.dot(Prior, Lik)
+  DocWordFreq_model /= DocWordFreq_model.sum(axis=1)[:,np.newaxis]
+  return DocWordFreq_model
+
+def calcKL_discrete(P1, P2):
+  KL = np.log(P1 + 1e-100) - np.log(P2 + 1e-100)
+  KL *= P1
+  return KL.sum()
