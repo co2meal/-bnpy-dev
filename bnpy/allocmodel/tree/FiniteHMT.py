Fixing respPair computation

bnpy/allocmodel/tree/FiniteHMT.py
@@ -9,16 +9,16 @@ class FiniteHMT(AllocModel):
     ######################################################### Constructors
     #########################################################
 
-    def __init__(self, inferType, allocPriorDict):
+    def __init__(self, inferType, priorDict=dict()):
         self.inferType = inferType
         self.K = 0
         self.initPi = None
         self.transPi = None
-        self.initAlpha = 0.0
-        self.maxBranch = 0
+        self.maxBranch = 4
+        self.set_prior(**priorDict)
 
-    def set_prior(self, initAlpha):
-        self.initAlpha = initAlpha
+    def set_prior(self, alpha0=1.0, **kwargs):
+        self.alpha0 = alpha0
 
     ######################################################### Local Params
     #########################################################
@@ -43,12 +43,11 @@ class FiniteHMT(AllocModel):
         respPair = LP['respPair']
         
         FirstStateCount = resp[0,:]
-        N = np.sum(resp, axis = 1)
+        N = np.sum(resp, axis = 0)
         SS = SuffStatBag(K = self.K , D = Data.dim)
         for b in xrange(self.maxBranch):
             PairCounts = np.sum(respPair[Data.mask[b],:,:], axis = 0)
             SS.setField('PairCounts'+str(b), PairCounts, dims=('K','K'))
-
         SS.setField('FirstStateCount', FirstStateCount, dims=('K'))
         SS.setField('N', N, dims=('K'))
 
@@ -63,12 +62,23 @@ class FiniteHMT(AllocModel):
             self.initPi = np.ones(self.K)
             self.transPi = np.ones((self.maxBranch, self.K, self.K))
 
-        self.initPi = (SS.FirstStateCount + self.initAlpha) / (SS.FirstStateCount.sum() + self.K * self.initAlpha)
+        self.initPi = (SS.FirstStateCount) / (SS.FirstStateCount.sum())
 
         for b in xrange(self.maxBranch):
             PairCounts = getattr(SS._Fields, 'PairCounts'+str(b))
             normFactor = np.sum(PairCounts, axis = 1)
-            self.transPi[b,:] = psiSums / normFactor[:,np.newaxis]
+            self.transPi[b,:,:] = PairCounts / normFactor[:,np.newaxis]
+
+    def init_global_params(self, Data, K=0, **kwargs):
+        self.K = K
+        branchNo = 4
+        if self.inferType == 'EM':
+            self.initPi = 1.0/K * np.ones(K)
+            self.transPi = np.empty((branchNo, K, K))
+            for b in xrange(branchNo):
+                self.transPi[b,:,:] = np.ones(K)[:,np.newaxis]/K * np.ones((K,K))
+        else:
+            print 'inferType other than EM are not yet supported for FiniteHMT'
 
     def set_global_params(self, hmodel=None, K=None, initPi=None, transPi=None, maxBranch=None,**kwargs):
         if hmodel is not None:
@@ -89,6 +99,18 @@ class FiniteHMT(AllocModel):
         if self.inferType == 'EM':
             return LP['evidence']
 
+    def from_dict(self, myDict):
+        self.inferType = myDict['inferType']
+        self.K = myDict['K']
+        if self.inferType == 'VB':
+            print 'VB is not supported yet for FiniteHMT'
+        elif self.inferType == 'EM':
+            self.initPit = myDict['initPi']
+            self.transPi = myDict['transPi']
+
     def to_dict(self):
         if self.inferType == 'EM':
             return dict(initPi = self.initPi, transPi = self.transPi)
+
+    def get_prior_dict(self):
+        return dict(alpha0=self.alpha0, K=self.K)

bnpy/allocmodel/tree/HMTUtil.py
@@ -41,18 +41,25 @@ def SumProductAlg_QuadTree(PiInit, PiMat, logSoftEv):
 
 	SoftEv, lognormC = expLogLik(logSoftEv)
 	umsg = UpwardPass(PiInit, PiMat, SoftEv)
-	dmsg, margPrObs = DownwardPass(PiInit, PiMat, SoftEv)
+	dmsg, margPrObs = DownwardPass(PiInit, PiMat, SoftEv, umsg)
 
 	respPair = np.zeros((N,K,K))
+
 	for n in xrange( 1, N ):
 		parent = get_parent_index(n)
-		branch = get_branch(n)
-    	respPair[n,:,:] = PiMat[branch,:,:] * np.outer(dmsg[parent], umsg[n] * SoftEv[n])
-    	respPair[n,:,:] = respPair[n,:,:] / np.sum(respPair[n,:,:])
+		siblings = get_children_indices(parent,N)
+		siblings.remove(n)
+		message = 1
+		message *= dmsg[parent]
+		for s in siblings:
+			branch = get_branch(s)
+			message *= np.dot(PiMat[branch,:,:], SoftEv[s]) * umsg[s]
+		respPair[n] = PiMat[get_branch(n),:,:] * np.outer(message, umsg[n] * SoftEv[n])
+		respPair[n] /= np.sum(respPair[n])
 
 	logMargPrSeq = np.log(margPrObs) + lognormC.sum()
-
 	resp = dmsg * umsg
+	resp = resp / resp.sum(axis=1)[:,np.newaxis]
 	return resp, respPair, logMargPrSeq
 
 
@@ -79,7 +86,6 @@ def UpwardPass(PiInit, PiMat, SoftEv):
 	'''
 	N = SoftEv.shape[0]
 	K = PiInit.size
-
 	umsg = np.ones( (N, K) )
 	start = find_last_nonleaf_node(N)
 	for n in xrange(start-1, -1, -1):
@@ -92,7 +98,7 @@ def UpwardPass(PiInit, PiMat, SoftEv):
 	return umsg
 
 
-def DownwardPass(PiInit, PiMat, SoftEv):
+def DownwardPass(PiInit, PiMat, SoftEv, umsg):
 	'''Propagate messages downwards along the tree, starting from the root
 
     Args
@@ -140,16 +146,16 @@ def DownwardPass(PiInit, PiMat, SoftEv):
 	return dmsg, margPrObs
 
 def get_parent_index(child_index):
-	if n == 0:
+	if child_index == 0:
 		return None #it is a root
-	elif n%4 == 0:
-		return (n-1)/4
+	elif child_index%4 == 0:
+		return (child_index-1)/4
 	else:
-		return n/4
+		return child_index/4
 
 def get_children_indices(parent, N):
 	if 4*parent+1 > N:
-		return None
+		return []
 	else:
 		myList = [4*parent+j+1 for j in range(4)]
 		return myList
@@ -158,7 +164,7 @@ def get_branch(child_index):
 	'''Find on which branch of its parent this child lies
 	'''
 	if (child_index%4 == 0):
-		return 4
+		return 3
 	else:
 		return (child_index%4 - 1)
 
@@ -170,7 +176,7 @@ def find_last_nonleaf_node(N):
 	else:
 		height = 1
 		total = 1
-		while (total + height*4) < N:
+		while (total + height**4) < N:
 			total += height*4
 			height += 1
 		return total

bnpy/allocmodel/tree/QuadTreeUtil.py
@@ -21,8 +21,14 @@ def calcRespBySumProduct(PiInit, PiMat, logSoftEv):
 	respPair = np.zeros((N,K,K))
 	for n in xrange( 1, N ):
 		parent = get_parent_index(n)
-		respPair[n,:,:] = PiMat * np.outer(dmsg[parent], umsg[n] * SoftEv[n])
-		respPair[n,:,:] = respPair[n,:,:] / np.sum(respPair[n,:,:])
+		siblings = get_children_indices(parent,N)
+		siblings.remove(n)
+		message = 1
+		message *= dmsg[parent]
+		for s in siblings:
+			message *= np.dot(PiMat, SoftEv[s]) * umsg[s]
+		respPair[n,:,:] = PiMat * np.outer(message, umsg[n] * SoftEv[n])
+		respPair[n,:,:] /= np.sum(respPair[n,:,:])
 
 	logMargPrSeq = np.log(margPrObs) + lognormC.sum()
 

bnpy/data/QuadTreeData.py
@@ -133,6 +133,6 @@ class QuadTreeData(TreeData, DataObj):
             return n/4
 
     def set_mask(self, nBranches):
-        self.mask = np.empty((nBranches, (self.nObs-1)/4))
+        self.mask = np.empty((nBranches, (self.nObs-1)/4), dtype=np.int32)
         for b in xrange(nBranches):
             self.mask[b,:] = [i for i in xrange(b+1, self.nObs, nBranches)]