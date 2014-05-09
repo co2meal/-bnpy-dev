Fix resp, respPair and logMargPrSeq calculation

bnpy/allocmodel/tree/FiniteHMT.py
@@ -59,7 +59,7 @@ class FiniteHMT(AllocModel):
         self.K = SS.K
 
         self.initPi = (SS.FirstStateCount) / (SS.FirstStateCount.sum())
-
+        
         for b in xrange(self.maxBranch):
             PairCounts = getattr(SS._Fields, 'PairCounts'+str(b))
             normFactor = np.sum(PairCounts, axis = 1)

bnpy/allocmodel/tree/HMTUtil.py
@@ -53,12 +53,13 @@ def SumProductAlg_QuadTree(PiInit, PiMat, logSoftEv):
 		message *= dmsg[parent]
 		for s in siblings:
 			branch = get_branch(s)
-			message *= np.dot(PiMat[branch,:,:], SoftEv[s]) * umsg[s]
+			message *= np.dot(PiMat[branch,:,:], umsg[s] * SoftEv[s])
 		respPair[n] = PiMat[get_branch(n),:,:] * np.outer(message, umsg[n] * SoftEv[n])
 		respPair[n] /= np.sum(respPair[n])
 
-	logMargPrSeq = np.log(margPrObs) + lognormC.sum()
+	#logMargPrSeq = np.log(margPrObs[N-1]) + lognormC.sum()
 	resp = dmsg * umsg
+	logMargPrSeq = np.log(resp[N-1].sum()) + lognormC.sum()
 	resp = resp / resp.sum(axis=1)[:,np.newaxis]
 	return resp, respPair, logMargPrSeq
 
@@ -94,7 +95,7 @@ def UpwardPass(PiInit, PiMat, SoftEv):
 			branch = get_branch(child)
 			umsg[n] = umsg[n] * np.dot(PiMat[branch,:,:], umsg[child]*SoftEv[child])
 		normalization_const = np.sum(umsg[n])
-		umsg[n] /= normalization_const
+		#umsg[n] /= normalization_const
 	return umsg
 
 
@@ -124,12 +125,13 @@ def DownwardPass(PiInit, PiMat, SoftEv, umsg):
 	for d in xrange(0, 4):
 		PiTMat[d,:,:] = PiMat[d,:,:].T
 
-	margPrObs = 0
+	margPrObs = np.empty( N )
 	dmsg = np.empty( (N,K) )
 	for n in xrange( 0, N ):
 		if n == 0:
 			dmsg[n] = PiInit * SoftEv[0]
-			margPrObs = np.sum(dmsg[n])
+			margPrObs[n] = np.sum(dmsg[n])
+			#dmsg[n] /= margPrObs[n]
 		else:
 			parent_index = get_parent_index(n)
 			siblings = get_children_indices(parent_index, N)
@@ -138,11 +140,11 @@ def DownwardPass(PiInit, PiMat, SoftEv, umsg):
 			message *= dmsg[parent_index]
 			for s in siblings:
 				branch = get_branch(s)
-				message *= np.dot(PiMat[branch,:,:], SoftEv[s]) * umsg[s]
+				message *= np.dot(PiMat[branch,:,:], umsg[s]* SoftEv[s])
 			branch = get_branch(n)
 			dmsg[n] = np.dot(PiTMat[branch,:,:], message) * SoftEv[n]
-		margPrObs = np.sum(dmsg[n])
-		dmsg[n] /= margPrObs
+		margPrObs[n] = np.sum(dmsg[n])
+		#dmsg[n] /= margPrObs[n]
 	return dmsg, margPrObs
 
 def get_parent_index(child_index):

bnpy/allocmodel/tree/QuadTreeUtil.py
@@ -26,13 +26,13 @@ def calcRespBySumProduct(PiInit, PiMat, logSoftEv):
 		message = 1
 		message *= dmsg[parent]
 		for s in siblings:
-			message *= np.dot(PiMat, SoftEv[s]) * umsg[s]
+			message *= np.dot(PiMat, umsg[s] * SoftEv[s])
 		respPair[n,:,:] = PiMat * np.outer(message, umsg[n] * SoftEv[n])
 		respPair[n,:,:] /= np.sum(respPair[n,:,:])
 
-	logMargPrSeq = np.log(margPrObs) + lognormC.sum()
-
+	#logMargPrSeq = np.log(np.sum(umsg[4]*margPrObs[N-1])) + lognormC.sum()
 	resp = dmsg * umsg
+	logMargPrSeq = np.log(resp[N-1].sum()) + lognormC.sum()
 	resp = resp / resp.sum(axis=1)[:,np.newaxis]
 	return resp, respPair, logMargPrSeq
 
@@ -53,7 +53,7 @@ def UpwardPass(PiInit, PiMat, SoftEv):
 		for child in children:
 			umsg[n] = umsg[n] * np.dot(PiMat, umsg[child] * SoftEv[child])
 		normalization_const = np.sum(umsg[n])
-		umsg[n] /= normalization_const
+		#umsg[n] /= normalization_const
 	return umsg
 
 
@@ -64,12 +64,12 @@ def DownwardPass(PiInit, PiMat, SoftEv, umsg):
 	K = PiInit.size
 	PiT = PiMat.T
 
-	margPrObs = 0
+	margPrObs = np.empty( N )
 	dmsg = np.empty( (N,K) )
 	for n in xrange( 0, N ):
 		if n == 0:
 			dmsg[n] = PiInit * SoftEv[0]
-			margPrObs = np.sum(dmsg[n])
+			margPrObs[n] = np.sum(dmsg[n])
 			#dmsg[n] /= margPrObs[n]
 		else:
 			parent_index = get_parent_index(n)
@@ -78,10 +78,10 @@ def DownwardPass(PiInit, PiMat, SoftEv, umsg):
 			message = 1
 			message *= dmsg[parent_index]
 			for s in siblings:
-				message *= np.dot(PiMat, SoftEv[s]) * umsg[s]
+				message *= np.dot(PiMat, umsg[s]* SoftEv[s])
 			dmsg[n] = np.dot(PiT, message) * SoftEv[n]
-			margPrObs = np.sum(dmsg[n])
-			dmsg[n] /= margPrObs
+			margPrObs[n] = np.sum(dmsg[n])
+			#dmsg[n] /= margPrObs[n]
 	return dmsg, margPrObs
 
 ########################################################### Brute Force

demodata/HMTK4.py
@@ -5,6 +5,8 @@ A module to create HMT data with 4 states
 '''
 import numpy as np
 from bnpy.data.QuadTreeData import QuadTreeData
+from bnpy.distr.GaussDistr import GaussDistr
+from bnpy.allocmodel.tree import HMTUtil
 
 K = 4
 D = 2
@@ -62,7 +64,16 @@ def get_data(seed=8675309, nObsTotal=256, **kwargs):
     X, Z, totalNodes = generateObservations(seed, nObsTotal)
     l = list()
     l.append(totalNodes-1)
-    trueParams = dict(initPi=pi0, transPi=transition, mu=means, Sigma=sigmas)
+    comp = list()
+    I = np.eye(2)
+    for b in range(K):
+        precMat = np.linalg.solve( sigmas[b], I )
+        comp.append(GaussDistr(m=means[b], L=precMat))
+    lpr = np.zeros( (341, K) )
+    for k in range(K):
+      lpr[:,k] = comp[k].log_pdf( QuadTreeData(X) )
+    resp, respPair, logMargPrSeq = HMTUtil.SumProductAlg_QuadTree(pi0, transition, lpr)
+    trueParams = dict(initPi=pi0, transPi=transition, mu=means, Sigma=sigmas, resp=resp, respPair=respPair)
     Data = QuadTreeData(X=X, TrueZ=Z, nTrees=1, tree_delims=l, TrueParams=trueParams)
     Data.summary = get_data_info()
     return Data

tests/allocmodel/tree/TestQuadTreeUtil.py
@@ -21,18 +21,18 @@ class TestQuadTreeUtil_5Nodes(unittest.TestCase):
 		print ''
 		resp, respPair, logPrObs = QTU.calcRespBySumProduct(
 									   self.PiInit, self.PiMat, self.logSoftEv)
-		resp2, respPair2, logPrObs2 = QTU.calcRespByBruteForce(
-										  self.PiInit, self.PiMat, self.logSoftEv)
+		#resp2, respPair2, logPrObs2 = QTU.calcRespByBruteForce(
+		#								  self.PiInit, self.PiMat, self.logSoftEv)
 		print '============================= resp'
 		print '------------- SumProduct'
 		print resp
-		print '------------- BruteForce'
-		print resp2
+		#print '------------- BruteForce'
+		#print resp2
 		print '============================= logPrObs'
 		print '------------- SumProduct'
 		print logPrObs
-		print '------------- BruteForce'
-		print logPrObs2
+		#print '------------- BruteForce'
+		#print logPrObs2
 
 class TestQuadTreeUtil_4States(TestQuadTreeUtil_5Nodes):
 
@@ -97,3 +97,17 @@ class TestQuadTreeUtil_NonuniformLik(TestQuadTreeUtil_5Nodes):
         self.PiMat = PiMat
         self.logSoftEv = logSoftEv
         self.SoftEv = SoftEv
+
+class TestQuadTreeUtil_NonuniformLik21(TestQuadTreeUtil_NonuniformLik):
+	def setUp(self):
+		PiInit = np.asarray([0.3, 0.7])
+		PiMat = np.asarray([[0.6, 0.4],
+							[0.2, 0.8]])
+		PRNG = np.random.RandomState(0)
+		p = PRNG.rand(21,1)
+		SoftEv = np.hstack([p, 1-p])
+		logSoftEv = np.log(SoftEv)
+		self.PiInit = PiInit
+		self.PiMat = PiMat
+		self.logSoftEv = logSoftEv
+		self.SoftEv = SoftEv