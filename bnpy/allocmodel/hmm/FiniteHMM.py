Fixed incorrect entropy calculation in FiniteHMM.py and added HDPHMM.py

bnpy/allocmodel/hmm/FiniteHMM.py
@@ -69,10 +69,10 @@ class FiniteHMM(AllocModel):
                                  digamma(np.sum(self.initTheta)))
 
             #Normalize so that FwdBwdAlg() will give accurate logMargPrSeq
-            expELogInit = expELogInit / np.sum(expELogInit)
-            for k in xrange(self.K):
-                expELogTrans[k, :] = expELogTrans[k, :] \
-                    / np.sum(expELogTrans[k,:])
+            #expELogInit = expELogInit / np.sum(expELogInit)
+            #for k in xrange(self.K):
+            #    expELogTrans[k, :] = expELogTrans[k, :] \
+            #        / np.sum(expELogTrans[k,:])
 
             initParam = expELogInit
             transParam = expELogTrans
@@ -161,6 +161,7 @@ class FiniteHMM(AllocModel):
         respPairSums = np.sum(respPair, axis = 0)
         firstStateResp = np.sum(resp[inds], axis = 0)
         N = np.sum(resp, axis = 0)
+
         SS = SuffStatBag(K = self.K , D = Data.dim)
         SS.setField('firstStateResp', firstStateResp, dims=('K'))
         SS.setField('respPairSums', respPairSums, dims=('K','K'))
@@ -258,7 +259,7 @@ class FiniteHMM(AllocModel):
         normQ = gammaln(np.sum(self.initTheta)) - \
             np.sum(gammaln(self.initTheta))
 
-        theMeat = np.sum((- self.initTheta) * 
+        theMeat = np.sum((self.initAlpha - self.initTheta) * 
                          (digamma(self.initTheta) - 
                           digamma(np.sum(self.initTheta))))
 
@@ -271,19 +272,27 @@ class FiniteHMM(AllocModel):
         normQ = np.sum(gammaln(np.sum(self.transTheta, axis = 1)) - \
                            np.sum(gammaln(self.transTheta), axis = 1))
 
-        theMeat = np.sum((- self.transTheta) *
+        theMeat = np.sum((self.initAlpha - self.transTheta) *
                          (digamma(self.transTheta) - 
-                              digamma(np.sum(self.transTheta, axis = 1))))
+                          digamma(np.sum(self.transTheta, axis = 1))[:,np.newaxis]))
         return normP - normQ + theMeat
  
     def elbo_z(self, LP, SS):
+        s = (LP['respPair'] / 
+             (np.sum(LP['respPair'], axis = 2)[:, :, np.newaxis] + EPS))
+
         z_1 = np.sum(LP['resp'][0,:] * (digamma(self.initTheta) - 
                                         digamma(np.sum(self.initTheta)) -
                                         np.log(LP['resp'][0,:] + EPS)))
+#        restZ = -np.sum(LP['respPair'][1:,:,:] * \
+#              np.log(LP['respPair'][1:,:,:] + EPS)) + \
+#              np.sum(digamma(self.transTheta)*SS.respPairSums) - \
+#              np.sum(digamma(np.sum(self.transTheta, axis = 1))*SS.respPairSums)
         restZ = -np.sum(LP['respPair'][1:,:,:] * \
-              np.log(LP['respPair'][1:,:,:] + EPS)) + \
+              np.log(s[1:,:,:] + EPS)) + \
               np.sum(digamma(self.transTheta)*SS.respPairSums) - \
               np.sum(digamma(np.sum(self.transTheta, axis = 1))*SS.respPairSums)
+
 
         return z_1 + restZ
         

demodata/SeqHMMK4.py
@@ -11,7 +11,7 @@ from bnpy.data import SeqXData, MinibatchIterator
 
 ##################################################### Set Parameters
 K = 4
-D = 20
+D = 2
 
 #transPi = np.asarray([[0.2, 0.2, 0.2, 0.4], \
 #                      [0.2, 0.6, 0.1, 0.1], \
@@ -31,18 +31,18 @@ mus = np.asarray([[0, 0], \
                   [10, 0], \
                   [0, 10], \
                   [10, 10]])
-mus = np.zeros((K,D))
-for i in xrange(K):
-  mus[i,i] = 10
+#mus = np.zeros((K,D))
+#for i in xrange(K):
+#  mus[i,i] = 10
 
 
 sigmas = np.empty((K,D,D))
-#sigmas[0,:,:] = np.asarray([[2, 0], [0, 2]])
-#sigmas[1,:,:] = np.asarray([[2, 0], [0, 2]])
-#sigmas[2,:,:] = np.asarray([[2, 0], [0, 2]])
-#sigmas[3,:,:] = np.asarray([[2, 0], [0, 2]])
-for i in xrange(K):
-  sigmas[i,:,:] = 2 * np.identity(D)
+sigmas[0,:,:] = np.asarray([[2, 0], [0, 2]])
+sigmas[1,:,:] = np.asarray([[2, 0], [0, 2]])
+sigmas[2,:,:] = np.asarray([[2, 0], [0, 2]])
+sigmas[3,:,:] = np.asarray([[2, 0], [0, 2]])
+#for i in xrange(K):
+#  sigmas[i,:,:] = 2 * np.identity(D)
 
 def get_minibatch_iterator(seed=8675309, dataorderseed=0, nBatch=10, nObsBatch=None, nObsTotal=25000, nLap=1, startLap=0, **kwargs):
   '''

tests/end-to-end/TestFHMMGauss.py
@@ -26,15 +26,15 @@ class TestHMMK4_EM(AbstractEndToEndTest):
         #Create true parameters
         self.K = 4
         mus = np.asarray([[0, 0], \
-                  [0, 100], \
-                  [100, 0], \
-                  [100, 100]])
+                  [0, 10], \
+                  [10, 0], \
+                  [10, 10]])
 
         sigmas = np.empty((4,2,2))
-        sigmas[0,:,:] = np.asarray([[10, 0], [0, 10]])
-        sigmas[1,:,:] = np.asarray([[10, 0], [0, 10]])
-        sigmas[2,:,:] = np.asarray([[10, 0], [0, 10]])
-        sigmas[3,:,:] = np.asarray([[10, 0], [0, 10]])
+        sigmas[0,:,:] = np.asarray([[2, 0], [0, 2]])
+        sigmas[1,:,:] = np.asarray([[2, 0], [0, 2]])
+        sigmas[2,:,:] = np.asarray([[2, 0], [0, 2]])
+        sigmas[3,:,:] = np.asarray([[2, 0], [0, 2]])
 
         #transPi = np.asarray([[0.0, 0.5, 0.0, 0.5], \
         #                          [0.25, 0.0, 0.5, 0.25], \
@@ -88,13 +88,9 @@ class TestHMMK4_EM(AbstractEndToEndTest):
             
         self.Data = bnpy.data.SeqXData(X, seqInds, Z)
         
-       #FiniteHMM finds precision matricies, so convert
-        for k in xrange(self.K):
-            sigmas[k,:,:] = np.linalg.inv(sigmas[k,:,:])
-
-        self.TrueParams = dict(K = self.K, m = mus, L = sigmas)
+        self.TrueParams = dict(K = self.K, m = mus, Sigma = sigmas)
                                    #transPi = transPi)
-        self.ProxFunc = dict(L = Util.CovMatProxFunc,
+        self.ProxFunc = dict(Sigma = Util.CovMatProxFunc,
                              m = Util.VectorProxFunc,
                              #transPi = Util.ProbMatrixProxFunc,
                              initPi = Util.ProbVectorProxFunc,
@@ -127,7 +123,7 @@ class TestHMMK4_VB(TestHMMK4_EM):
 
     def setup(self):
         super(TestHMMK4_VB, self).setup()
-        self.learnAlgs = ['VB', 'EM', 'soVB', 'moVB']
+        self.learnAlgs = ['VB', 'soVB', 'moVB']
 
         
         