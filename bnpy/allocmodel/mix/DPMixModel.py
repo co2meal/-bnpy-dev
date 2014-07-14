ENH fixed DiagGauss calcPredProbVec so more numerically stable. earlier versions could result with prob vec that had NaN values

bnpy/allocmodel/mix/DPMixModel.py
@@ -164,8 +164,14 @@ class DPMixModel(AllocModel):
       alloc_prob = self.getConditionalProbVec_Unnorm(SS, doKeepFinalCompEmpty)
       pvec = obsModel.calcPredProbVec_Unnorm(SS, x)
       pvec *= alloc_prob
-      pvec /= np.sum(pvec)
+      psum = np.sum(pvec)
 
+      if np.isnan(psum) or psum <= 0:
+        print pvec
+        print psum
+        raise ValueError('BAD VALUES FOR PROBS!')
+
+      pvec /= psum
       # sample new allocation
       knew = PRNG.choice(SS.K, p=pvec)
 

bnpy/obsmodel/DiagGaussObsModel.py
@@ -513,9 +513,12 @@ class DiagGaussObsModel(AbstractObsModel):
     base += 1
     logp = (-0.5 * (nu+1))[:,np.newaxis] * np.log(base)
     logp += (gammaln(0.5 * (nu+1)) - gammaln(0.5 * nu))[:,np.newaxis]
-    p = logp
-    np.exp(logp, out=p)
-    p /= np.sqrt(kbeta)
+    logp -= 0.5 * np.log(kbeta)
+    logp -= np.max(logp)
+    p = np.exp(logp)
+    #p = logp
+    #np.exp(logp, out=p)
+    #p /= np.sqrt(kbeta)
     return np.prod(p, axis=1)
 
   def _calcPredProbVec_ForLoop(self, SS, x):