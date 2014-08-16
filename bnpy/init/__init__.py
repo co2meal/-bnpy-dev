1684f5f527693177823a3a0a986023e9d6284cc7
Mike Hughes <mike@michaelchughes.com>
2014-08-16 11:18:02 -0400

    ADD FromLP initialization, so we can do use the contiguous blocks init using in previous work on mocap data (Hughes, Fox, Sudderth et al). Syntax: --initname contigblocksLP

--- bnpy/init/__init__.py
+++ bnpy/init/__init__.py
@@ -3,6 +3,6 @@ The :mod:`init` module gathers initialization procedures for model parameters
 """
 import FromScratchGauss, FromScratchMult
 import FromScratchBernRel
-import FromSaved, FromTruth
+import FromSaved, FromTruth, FromLP
 
-__all__ = ['FromScratchGauss', 'FromSaved', 'FromTruth', 'FromScratchMult', 'FromScratchBernRel']
+__all__ = ['FromScratchGauss', 'FromSaved', 'FromTruth', 'FromScratchMult', 'FromScratchBernRel', 'FromLP']
