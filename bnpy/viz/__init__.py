1712a1d9a011db57f71e984265f45dc6d670ef46
Mike Hughes <mike@michaelchughes.com>
2014-09-13 16:56:07 -0400

    ENH revamped PlotELBO and PlotK scripts to work with new-style <dataName>/<jobpath>/ saving format... can easily add custom keyword args to narrow down a set of jobs to plot

--- bnpy/viz/__init__.py
+++ bnpy/viz/__init__.py
@@ -5,6 +5,7 @@ The :mod:`viz` module provides visualization capability
 import GaussViz
 import BarsViz
 import PlotELBO
+import PlotK
 import PlotComps
 
-__all__ = ['GaussViz', 'BarsViz', 'PlotELBO', 'PlotComps']
+__all__ = ['GaussViz', 'BarsViz', 'PlotELBO', 'PlotComps', 'PlotK']
