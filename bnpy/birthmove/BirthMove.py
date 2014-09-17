d34d5319460f0ef40567f0c76f9c842d7d6411fb
Mike Hughes <mhughes@cs.brown.edu>
2014-09-17 09:04:59 -0400

    ENH improved logging for DPMix births that target specific comps

--- bnpy/birthmove/BirthMove.py
+++ bnpy/birthmove/BirthMove.py
@@ -19,7 +19,15 @@ def run_birth_move(bigModel, bigSS, freshData, Q=None, Plan=None, **kwargsIN):
       MoveInfo
   '''
   logPhase('Target Data')
+  if 'ktarget' in Plan:
+    ktarget = Plan['ktarget']
+    if hasattr(bigSS, 'uIDs'):
+      uid = bigSS.uIDs[ktarget]
+      log('ktarget= %d. uID= %d.' % (ktarget, uid))
+    else:
+      log('ktarget= %d.' % (ktarget))
   log(freshData.get_stats_summary())
+
 
   kwargs = dict(**kwargsIN) # make local copy!
   origids = dict( bigModel=id(bigModel), bigSS=id(bigSS) )
