2262c30dff0e315d2366038d2ab8afaa83e7469b
Mike Hughes <mike@michaelchughes.com>
2014-10-07 15:22:29 -0400

    ENH MergePlanner cuts off any pair with correlation less than 0.05, and will avoid badness with corrLimitDegree if there are no viable candidates

--- bnpy/mergemove/MergePlanner.py
+++ bnpy/mergemove/MergePlanner.py
@@ -353,7 +353,7 @@ def calcScoreMatrix_wholeELBO(curModel, SS, excludePairs=list(), M=None):
 
 ########################################################### Correlation cues
 ###########################################################
-def calcScoreMatrix_corr(SS, MINVAL=1e-8):
+def calcScoreMatrix_corr(SS, MINCORR=0.05, MINVAL=1e-8):
   '''
      Returns
      -------
@@ -379,9 +379,10 @@ def calcScoreMatrix_corr(SS, MINVAL=1e-8):
   CorrMat = CovMat / np.outer(sqrtc, sqrtc)
 
   # Now, filter to leave only *positive* entries in upper diagonal
-  #  we shouldn't even bother trying to merge topics with neg correlations
+  #  we shouldn't even bother trying to merge topics
+  #  with negative or nearly zero correlations
   CorrMat[np.tril_indices(K)] = 0
-  CorrMat[CorrMat < 0] = 0
+  CorrMat[CorrMat < MINCORR] = 0
   CorrMat[nanIDs] = 0
 
   return CorrMat
@@ -406,6 +407,8 @@ def calcScoreMatrix_corrLimitDegree(SS, MINCORR=0.05, N=3):
     pairIDs = selectPairsUsingAtMostNOfEachComp(A, fixedPairIDs, N=N)
     fixedPairIDs = fixedPairIDs + pairIDs
   Mlimit = np.zeros_like(M)
+  if len(fixedPairIDs) == 0:
+    return Mlimit
   x,y = zip(*fixedPairIDs)
   Mlimit[x,y] = M[x,y]
   return Mlimit
