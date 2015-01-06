e7d65790d6c3079573188e6f83f40b93f5b6ac23
William Stephenson <william_stephenson@brown.edu>
2015-01-06 13:47:11 -0500

    Added comment with example useage

--- bnpy/viz/SequenceViz.py
+++ bnpy/viz/SequenceViz.py
@@ -1,3 +1,22 @@
+'''
+SequenceViz.py
+Visualizes sequential data as a plot of colored bars of the estimated state
+as calculated by the Viterbi algorithm.  Note that this script requires you
+to have made a run of bnpy with the custom func learnalg/extras/XViterbi.py
+running.
+
+Example use:
+
+python SequenceViz.py --dataset MoCap6 --jobname defaultjob --taskid 1
+                        --lap 5 --sequences 1,2,4,6
+
+
+
+
+'''
+
+
+
 import scipy.io
 import os
 import matplotlib.pyplot as plt
