5450f47d28cc4332c6994e42bf79e251b05ae985
Mert Terzihan <mert_terzihan@brown.edu>
2015-01-15 16:21:09 -0500

    Load data from matlab code's graph connectivity matrices

--- datasets/pepperHMT.py
+++ datasets/pepperHMT.py
@@ -8,15 +8,30 @@ from bnpy.data import GroupXData
 from bnpy.distr.GaussDistr import GaussDistr
 from bnpy.allocmodel.tree import HMTUtil
 from scipy.io import loadmat
+from collections import deque
 
-path = '/Users/mertterzihan/Desktop/pepperimd256.mat'
+path = '/Users/mertterzihan/Desktop/pepper256.mat'
 
 def get_data(seed=8675309, nObsTotal=25000, **kwargs):
+  q = deque()
   d = loadmat(path)
-  graph = np.zeros(d['graph'].shape)
-  graph[:] = d['graph']
-  doc_range = np.zeros(d['doc_range'].shape)
-  doc_range[:] = d['doc_range']
+  datass_graph = np.zeros(d['datass_graph'].shape)
+  datass_graph[:] = d['datass_graph']
+  children_matrix = np.zeros(d['children_matrix'].shape)
+  children_matrix[:] = d['children_matrix']
+  graph = np.zeros([datass_graph.shape[1], datass_graph.shape[0]])
+  idx = 0
+  doc_range = np.zeros([401, 1])
+  for ii in xrange(400):
+		q.append(ii)
+		while len(q) > 0:
+			curr = q.popleft()
+			graph[idx,:] = datass_graph[:,curr]
+			idx += 1
+			ch_idx = children_matrix[curr, :]-1
+			if ~(ch_idx==-1).any():
+				q.extend(ch_idx.tolist())
+		doc_range[ii+1] = idx
   return GroupXData(graph, doc_range)
 
 def get_data_info():
