dd6a333d49ff6ca6722eab4972cd8563df1dad77
Mike Hughes <mike@michaelchughes.com>
2015-01-20 10:24:13 -0500

    ENH added capabilities for making figures in Fig 1 of AISTATS paper. TestSurrogateBound makes the bounds figure, while TestELBOPenalizesEmptyComps makes the ELBO change vs num empty figure.

--- tests/allocmodel/topics/TestSurrogateBound.py
+++ tests/allocmodel/topics/TestSurrogateBound.py
@@ -17,9 +17,10 @@ from scipy.special import gammaln
 import unittest
 import bnpy
 
-TICKSIZE=20
-FONTSIZE=25
-
+LEGENDSIZE=30
+TICKSIZE=30
+FONTSIZE=40
+LINEWIDTH=4
 
 def cD_exact(alphaVals, beta1):
   return gammaln(alphaVals) \
@@ -54,10 +55,14 @@ def plotErrorVsAlph(alphaVals=np.linspace(.001, 3, 1000),
   boundVals = cD_bound(alphaVals, beta1)
   assert np.all(exactVals >= boundVals)
   pylab.plot(alphaVals, exactVals - boundVals, 
-              '-', linewidth=2, label='beta_1=%.2f' % (beta1))
+              '-', linewidth=LINEWIDTH, label='beta_1=%.2f' % (beta1))
 
+  pylab.xlim([np.min(alphaVals)-0.1, np.max(alphaVals)+0.1])
+  pylab.xticks(np.arange(np.max(alphaVals)+1))
   pylab.xlabel("alpha", fontsize=FONTSIZE)
+
   pylab.ylabel("error", fontsize=FONTSIZE)
+  pylab.yticks(np.arange(0, 1.5, 0.5))
   pylab.tick_params(axis='both', which='major', labelsize=TICKSIZE)
   
 
@@ -67,37 +72,46 @@ def plotBoundVsAlph(alphaVals=np.linspace(.001, 3, 1000),
   boundVals = cD_bound(alphaVals, beta1)
 
   assert np.all(exactVals >= boundVals)
-  pylab.plot( alphaVals, exactVals, 'k-', linewidth=2)
-  pylab.plot( alphaVals, boundVals, 'r--', linewidth=2)
+  pylab.plot( alphaVals, exactVals, 'k-', linewidth=LINEWIDTH)
+  pylab.plot( alphaVals, boundVals, 'r--', linewidth=LINEWIDTH)
   pylab.xlabel("alpha", fontsize=FONTSIZE)
+  pylab.ylabel("  ", fontsize=FONTSIZE)
+  pylab.xlim([np.min(alphaVals)-0.1, np.max(alphaVals)+0.1])
+  pylab.ylim([np.min(exactVals)-0.05, np.max(exactVals)+0.05])
+  pylab.xticks(np.arange(np.max(alphaVals)+1))
 
-  pylab.legend(['c_D exact', 'c_D surrogate'], fontsize=FONTSIZE, loc='lower right')
+  pylab.legend(['c_D exact', 'c_D surrogate'], fontsize=LEGENDSIZE, loc='lower right')
   pylab.tick_params(axis='both', which='major', labelsize=TICKSIZE)
 
 if __name__ == '__main__':
   from matplotlib import pylab
-  # Set buffer-space for defining plotable area
-  B = 0.15 # big buffer for sides where we will put text labels
-  b = 0.05 # small buffer for other sides
+  pylab.rcParams['ytick.major.pad']='8'
 
-  fig1 = pylab.figure(figsize=(8, 6))
+  # Set buffer-space for defining plotable area
+  B = 0.18 # big buffer for sides where we will put text labels
+  b = 0.02 # small buffer for other sides
+
+  fig1 = pylab.figure(figsize=(10, 6))
   axH = pylab.subplot(111)  
   axH.set_position([B, B, (1-B-b), (1-B-b)])
   plotBoundVsAlph(beta1=0.5)
-  
-  fig2 = pylab.figure(figsize=(8, 6))
+
+  fig2 = pylab.figure(figsize=(10, 6))
   axH = pylab.subplot(111)  
   axH.set_position([B, B, (1-B-b), (1-B-b)])
   plotErrorVsAlph(beta1=0.5)
   plotErrorVsAlph(beta1=0.25) 
   plotErrorVsAlph(beta1=0.05)
   plotErrorVsAlph(beta1=0.01)
-  pylab.legend(loc='upper left', fontsize=FONTSIZE)
+  pylab.legend(loc='upper left', fontsize=LEGENDSIZE)
 
-  #pylab.figure(fig1)
-  #pylab.savefig('SurrogateBound_cDVsAlpha.eps', bbox_inches='tight')
-  #pylab.figure(fig2)
-  #pylab.savefig('SurrogateBound_ErrorVsAlpha.eps', bbox_inches='tight')
+  pylab.show(block=False)
+  keypress = raw_input('Press y to save, any other key to close >>')
+  if keypress.count('y'):
+    pylab.figure(1)
+    pylab.savefig('SurrogateBound_cDVsAlpha.eps', bbox_inches='tight', format='eps')
+    pylab.figure(2)
+    pylab.savefig('SurrogateBound_ErrorVsAlpha.eps', bbox_inches='tight')
 
   pylab.show(block=True)
   
