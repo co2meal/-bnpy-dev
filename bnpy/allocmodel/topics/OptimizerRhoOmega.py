47fe6bdaaa1e358952330efa807ed718a2ef24de
William Stephenson <william_stephenson@brown.edu>
2015-01-21 14:56:08 -0500

    removed a stray parenthesis

--- bnpy/allocmodel/topics/OptimizerRhoOmega.py
+++ bnpy/allocmodel/topics/OptimizerRhoOmega.py
@@ -231,13 +231,13 @@ def objFunc_constrained(rhoomega,
   Elogu = digamma(g1) - digammaomega
   Elog1mu = digamma(g0) - digammaomega
 
-  # TODO : when is nDoc=0 or 1?  I actually use nDoc-1 when we have kappa, but
+  # TODO : when is nDoc=0 or 1?  I use nDoc-1 when we have kappa, but
   #  when kappa is in use, there's always at least two "documents" (sticks), so
   #  this shouldn't crash in the short term.
   if nDoc > 0:
     if kappa > 0:
       scale = nDoc - 1
-      OFFcoef = kvec(K) + (gamma - g0)/scale + 1/(K*scale))
+      OFFcoef = kvec(K) + (gamma - g0)/scale + 1/(K*scale)
       Tvec = sumLogPi/scale + K * (np.log(alpha+kappa) - np.log(kappa))
     else:
       scale = nDoc
