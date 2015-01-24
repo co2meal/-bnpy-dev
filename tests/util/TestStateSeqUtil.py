9495df378666c9bbdc088d85ed48bbe5d3b49e5a
Mike Hughes <mike@michaelchughes.com>
2015-01-24 13:01:29 -0500

    ENH small fixes in tests to make sure we handle alignment right when there are empty states in the est sequence. Also, GroupXData fixed so saving to mat records the true labels TrueZ

--- tests/util/TestStateSeqUtil.py
+++ tests/util/TestStateSeqUtil.py
@@ -123,3 +123,37 @@ class TestStateSeqUtil(unittest.TestCase):
     distA = SSU.calcHammingDistance(zA, zTru)
     distExp = SSU.calcHammingDistance(zExp, zTru)
     assert distA == distExp
+
+
+  def test_alignEstStateSeqToTrue__Kest_gt_Ktrue_someempty(self):
+    ''' Verify alignment works when est sequence has more states than true
+    '''
+    print ''
+
+    zEst = [1,1,1,2,2,1,1,1]
+    zTru = [0,0,0,0,0,0,0,0]
+    zExp = [0,0,0,2,2,0,0,0]
+    zA = SSU.alignEstimatedStateSeqToTruth(zEst, zTru)
+    assert np.allclose(zA, zExp)
+    hdist = SSU.calcHammingDistance(zA, zTru)
+    print hdist
+    assert hdist == 2
+
+    zEst = [2,2,2,3,4,5,2,2]
+    zTru = [0,0,0,0,0,0,0,0]
+    zExp = [0,0,0,3,4,5,0,0]
+    zA = SSU.alignEstimatedStateSeqToTruth(zEst, zTru)
+    assert np.allclose(zA, zExp)
+    hdist = SSU.calcHammingDistance(zA, zTru)
+    print hdist
+    assert hdist == 3
+
+
+    zEst = [2,2,2,3,4,5,2,2]
+    zTru = [1,1,0,0,0,0,0,0]
+    zExp = [0,0,0,4,5,1,0,0]
+    zA = SSU.alignEstimatedStateSeqToTruth(zEst, zTru)
+    print zA
+    assert np.allclose(zA, zExp)
+    hdist = SSU.calcHammingDistance(zA, zTru)
+    assert hdist == 5
