f96d195d6c404a5bdb46302599e681a29e734f68
Mike Hughes <mike@michaelchughes.com>
2015-01-28 14:53:11 -0500

    ENH have basic C++ version of respPair-based summaries implemented and fully tested.

--- bnpy/allocmodel/hmm/lib/FwdBwdRowMajor.cpp
+++ bnpy/allocmodel/hmm/lib/FwdBwdRowMajor.cpp
@@ -26,6 +26,18 @@ extern "C" {
     double * bwdMsgOUT,
     int K,
     int T);
+
+  void SummaryAlg(
+    double * initPiIN,
+    double * transPiIN,
+    double * SoftEvIN,
+    double * margPrObsIN,
+    double * fwdMsgIN,
+    double * bwdMsgIN,
+    double * TransStateCountOUT,
+    double * HtableOUT,
+    int K,
+    int T);
 }
 
 
@@ -112,4 +124,86 @@ void BwdAlg(
         bMsg.row(t) /= margPrObs(t+1);
     }
 
+}
+
+
+// ======================================================== Summary Algorithm
+// ======================================================== 
+void SummaryAlg(
+    double * initPiIN,
+    double * transPiIN,
+    double * SoftEvIN,
+    double * margPrObsIN,
+    double * fwdMsgIN,
+    double * bwdMsgIN,
+    double * TransStateCountOUT,
+    double * HtableOUT,
+    int K,
+    int T)
+{
+    // Prep input
+    ExtArr1D initPi (initPiIN, K);
+    ExtArr2D transPi (transPiIN, K, K);
+    ExtArr2D SoftEv (SoftEvIN, T, K);
+    ExtArr1D margPrObs (margPrObsIN, T);
+    ExtArr2D fwdMsg (fwdMsgIN, T, K);
+    ExtArr2D bwdMsg (bwdMsgIN, T, K);
+
+    // Prep output
+    ExtArr2D TransStateCount (TransStateCountOUT, K, K);
+    ExtArr2D Htable (HtableOUT, K, K);
+  
+    // Temporary KxK array for storing respPair at timestep t
+    Arr2D respPair_t = ArrayXXd::Zero(K, K);
+    Arr1D logrowwiseSum = ArrayXd::Zero(K);
+
+    Arr2D epsArr = 1e-100 * ArrayXXd::Ones(K, K);
+
+    for (int t = 1; t < T; t++) {
+        // In Python, we want:
+        // >>> respPair[t] = np.outer(fmsg[t-1], bmsg[t] * SoftEv[t])
+        // >>> respPair[t] *= PiMat / margPrObs[t]
+        respPair_t = fwdMsg.row(t-1).transpose().matrix() \
+                      * (bwdMsg.row(t) * SoftEv.row(t)).matrix();
+        respPair_t *= transPi;
+        respPair_t /= margPrObs(t);
+
+
+        // Aggregate pairwise transition counts
+        TransStateCount += respPair_t;
+
+
+        // Aggregate entropy in a KxK matrix
+
+        // Step 1/3: Make numerically safe for logarithms
+        // Each entry in respPair_t will be at least eps (1e-100)
+        // Remember, cwiseMax only works with arrays, not scalars :(
+        // https://forum.kde.org/viewtopic.php?f=74&t=98384
+        respPair_t = respPair_t.cwiseMax(epsArr);
+
+        // Step 2/3: Increment by rP log rP
+        Htable += respPair_t * respPair_t.log();
+
+        // Step 3/3: Decrement by rP log rP.rowwise().sum()
+        // Remember, broadcasting with *= doesnt work
+        // https://forum.kde.org/viewtopic.php?f=74&t=95629 
+        // so we use a forloop instead
+        logrowwiseSum = respPair_t.rowwise().sum();
+        logrowwiseSum = logrowwiseSum.log();
+        for (int k=0; k < K; k++) {
+          respPair_t.col(k) *= logrowwiseSum;
+        }
+        Htable -= respPair_t; 
+
+        /*
+        printf("----------- t=%d\n", t);
+        for (int j = 0; j < K; j++) {
+          for (int k = 0; k < K; k++) {
+            printf(" %.3f", respPair_t(j,k));
+          }
+          printf("\n");
+        }
+        */
+    }
+    Htable *= -1.0;
 }
