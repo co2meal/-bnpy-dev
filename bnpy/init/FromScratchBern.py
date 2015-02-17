8fa937214bbcdb81a5aeb772b992586ea802ed14
Mike Hughes <mike@michaelchughes.com>
2015-02-17 12:15:13 -0500

    ENH BernObsModel works for both EM and VB and moVB. Visualization of learned parameters is now in BernViz.py

--- bnpy/init/FromScratchBern.py
+++ bnpy/init/FromScratchBern.py
@@ -1,7 +1,8 @@
 '''
-FromScratchGauss.py
+FromScratchBern.py
 
-Initialize params of an HModel with gaussian observations from scratch.
+Initialize global params of Bernoulli data-generation model, from scratch.
+
 '''
 
 import numpy as np
@@ -14,7 +15,7 @@ def init_global_params(obsModel, Data, K=0, seed=0,
                                        initname='randexamples',
                                        initBlockLen=20,
                                        **kwargs):
-  ''' Initialize parameters for Gaussian obsModel, in place.
+  ''' Initialize parameters for Bernoulli obsModel, in place.
 
       Returns
       -------
@@ -29,6 +30,7 @@ def init_global_params(obsModel, Data, K=0, seed=0,
     permIDs = PRNG.permutation(Data.nObs).tolist()
     for k in xrange(K):
       resp[permIDs[k],k] = 1.0
+
   elif initname == 'randexamplesbydist':
     # Choose K items from the Data,
     #  selecting the first at random,
@@ -44,6 +46,8 @@ def init_global_params(obsModel, Data, K=0, seed=0,
     resp = np.zeros((Data.nObs, K))
     for k in xrange(K):
       resp[chosenObjIDs[k], k] = 1.0
+
+
   elif initname == 'randcontigblocks':
     # Choose K contig blocks of provided size from the Data,
     #  selecting each block at random from a particular sequence
@@ -68,52 +72,22 @@ def init_global_params(obsModel, Data, K=0, seed=0,
         b = a + initBlockLen
       resp[a:b, k] = 1.0
 
-  elif initname == 'randsoftpartition':
-    # Randomly assign all data items some mass in each of K components
-    #  then create component params by M-step given that soft partition
-    resp = PRNG.gamma(1.0/(K*K), 1, size=(Data.nObs, K))
-    resp[resp < 1e-3] = 0
-    rsum = np.sum(resp,axis=1)
-    badIDs = rsum < 1e-8
-    if np.any(badIDs): # if any rows have no content, just set them to unif resp.
-      resp[badIDs] = 1.0 / K
-      rsum[badIDs] = 1
-    resp = resp/rsum[:,np.newaxis]
-    assert np.allclose(np.sum(resp, axis=1), 1.0)
-
-  elif initname == 'randparams':
-    CovMat = np.diag(np.cov(Data.X.T))
-    mu = np.sqrt(CovMat) * PRNG.randn(K, Data.dim)
-    Sigma = obsModel.get_covar_mat_for_comp('prior')
-    Sigma = np.tile(Sigma, (K, 1, 1))
-    obsModel.setEstParams(mu=mu, Sigma=Sigma)
-    if obsModel.inferType != 'EM':
-      obsModel.setPostFromEstParams(obsModel.EstParams, N=Data.nObs)
-      del obsModel.EstParams
-    return
-
-  elif initname == 'randomnaive':
-    # Generate K "fake" examples from the diagonalized data covariance,
-    #  creating params by assigning each "fake" example to a component.
-    Sig = np.sqrt(np.diag(np.cov(Data.X.T)))
-    Xfake = Sig * PRNG.randn(K, Data.dim)
-    Data = XData(Xfake)
-    resp = np.eye(K)
-
   elif initname == 'kmeans':
     # Fill in resp matrix with hard-clustering from K-means
     # using an initialization with K randomly selected points from X
     np.random.seed(seed)
-    centroids, labels = kmeans2(data = Data.X, k = K, minit = 'points')
+    centroids, labels = kmeans2(data=Data.X, k=K, minit='points')
     resp = np.zeros((Data.nObs, K))
-    for t in xrange(Data.nObs):
-      resp[t,labels[t]] = 1
+    for n in xrange(Data.nObs):
+      resp[n, labels[n]] = 1
 
   else:
     raise NotImplementedError('Unrecognized initname ' + initname)
 
+  # Using the provided resp for each token,
+  # we summarize into sufficient statistics
+  # then perform one global step (M step) to get initial global params 
   tempLP = dict(resp=resp)
   SS = SuffStatBag(K=K, D=Data.dim)
   SS = obsModel.get_global_suff_stats(Data, SS, tempLP)
   obsModel.update_global_params(SS)
-
--- bnpy/init/__init__.py
+++ bnpy/init/__init__.py
@@ -7,3 +7,4 @@ import FromTruth
 import FromLP
 import FromScratchGauss
 import FromScratchMult
+import FromScratchBern
