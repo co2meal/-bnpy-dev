5c3eecd19c2bea3d5a2197d35d162645226a3411
Mike Hughes <mike@michaelchughes.com>
2015-02-22 08:33:04 -0500

    MAINT Run.py and __init__.py updated to PEP8 compliant

--- bnpy/__init__.py
+++ bnpy/__init__.py
@@ -12,59 +12,59 @@ import obsmodel
 from HModel import HModel
 
 import ioutil
-load_model = ioutil.ModelReader.load_model
-save_model = ioutil.ModelWriter.save_model
-
 import init
 import learnalg
 import birthmove
 import mergemove
-#import deletemove
+import deletemove
 
 import Run
-from Run import run
 
-########################################################### Configure save
-###########################################################  location
+run = Run.run
+load_model = ioutil.ModelReader.load_model
+save_model = ioutil.ModelWriter.save_model
+
+# Configure save location
 hasWriteableOutdir = False
 if 'BNPYOUTDIR' in os.environ:
-  outdir = os.environ['BNPYOUTDIR']
-  if os.path.exists(outdir):
-    try:
-      testfilepath = os.path.join(outdir, '.bnpy-test-write-permissions.txt')
-      with open(testfilepath, 'w') as f:
-        pass
-    except IOError:
-      sys.exit('BNPYOUTDIR not writeable: %s' % (outdir))
-    hasWriteableOutdir = True
+    outdir = os.environ['BNPYOUTDIR']
+    if os.path.exists(outdir):
+        try:
+            testfilepath = os.path.join(
+                outdir, '.bnpy-test-write-permissions.txt')
+            with open(testfilepath, 'w') as f:
+                pass
+        except IOError:
+            sys.exit('BNPYOUTDIR not writeable: %s' % (outdir))
+        hasWriteableOutdir = True
 if not hasWriteableOutdir:
-  raise ValueError('Environment variable BNPYOUTDIR not specified. Cannot save results to disk')
+    raise ValueError(
+        'Environment variable BNPYOUTDIR not specified.' +
+        ' Cannot save results to disk')
 
-########################################################### Configure data
-###########################################################  location
+# Configure PYTHONPATH
 root = os.path.sep.join(os.path.abspath(__file__).split(os.path.sep)[:-2])
 sys.path.append(os.path.join(root, 'datasets/'))
+sys.path.append(os.path.join(root, 'third-party/'))
+sys.path.append(os.path.join(root, 'third-party/anchorwordtopics/'))
+
 if 'BNPYDATADIR' in os.environ:
-  if os.path.exists(os.environ['BNPYDATADIR']):
-    sys.path.append(os.environ['BNPYDATADIR'])
-  else:
-    print "Warning: Environment variable BNPYDATADIR not a valid directory"
+    if os.path.exists(os.environ['BNPYDATADIR']):
+        sys.path.append(os.environ['BNPYDATADIR'])
+    else:
+        print "Warning: Environment variable BNPYDATADIR not a valid directory"
 
-########################################################### Optional: viz
-###########################################################  package for plots
-canPlot = False
 
+__all__ = ['run', 'Run', 'learnalg', 'allocmodel', 'obsmodel', 'suffstats',
+           'HModel', 'init', 'util', 'ioutil']
+
+# Optional viz package for plotting
 try:
-  from matplotlib import pylab
-  canPlot = True
+    from matplotlib import pylab
+    import viz
+    canPlot = True
+    __all__.append('viz')
 except ImportError:
-  print "Error importing matplotlib. Plotting disabled."
-  print "Fix by making sure this produces a figure window on your system"
-  print " >>> from matplotlib import pylab; pylab.figure(); pylab.show();"
-
-__all__ = ['run', 'Run', 'learn', 'allocmodel','obsmodel', 'suffstats',
-           'HModel', 'init', 'util', 'ioutil', 'mergeutil']
-
-if canPlot:
-  import viz
-  __all__.append('viz')
+    print "Error importing matplotlib. Plotting disabled."
+    print "Fix by making sure this produces a figure window on your system"
+    print " >>> from matplotlib import pylab; pylab.figure(); pylab.show();"
