fb8da70f36957b310ba4bfd8aef88cf04c5f957b
Mike Hughes <mike@michaelchughes.com>
2015-03-04 16:51:35 -0500

    ENH improved parallel tests for kmeans dummy example

--- tests/parallel/TestKMeans.py
+++ tests/parallel/TestKMeans.py
@@ -1,10 +1,22 @@
-import numpy as np
-import bnpy
-from scipy.spatial.distance import cdist
+import os
+import multiprocessing
+from multiprocessing import sharedctypes
 
-import dill
-from pathos.multiprocessing import Pool
+import numpy as np
+from scipy.spatial.distance import cdist
 import unittest
+import ctypes
+import bnpy
+
+def getPtrForArray(X):
+    """ Get int pointer to memory location of provided array
+
+    Returns
+    --------
+    ptr : int
+    """
+    ptr, read_only_flag = X.__array_interface__['data']
+    return int(ptr)
 
 def localStep_Vectorized(X, Mu, start=None, stop=None):
   ''' K-means step
@@ -18,13 +30,12 @@ def localStep_Vectorized(X, Mu, start=None, stop=None):
   # Unpack size variables
   K, D = Mu.shape
 
-  # Assign each row of X to closest row of Mu
   if start is not None:
-    print 'WORKING ON CHUNK %d:%d' % (start, stop)
     Xcur = X[start:stop]
   else:
     Xcur = X
 
+  # Assign each row of X to closest row of Mu
   Dist = cdist(Xcur, Mu,'sqeuclidean')
   Z = Dist.argmin(axis=1)
 
@@ -40,6 +51,35 @@ def localStep_Vectorized(X, Mu, start=None, stop=None):
   SS.setField('DataStatVec', DataStatVec, dims=('K','D'))
   return SS
 
+class Worker(multiprocessing.Process):
+    def __init__(self, uid, JobQueue, ResultQueue):
+        super(Worker, self).__init__()
+        self.uid = uid
+        self.JobQueue = JobQueue
+        self.ResultQueue = ResultQueue
+
+    def printMsg(self, msg):
+        for line in msg.split("\n"):
+            print "#%d: %s" % (self.uid, line)
+
+    def run(self):
+        self.printMsg("process SetUp! pid=%d" % (os.getpid()))
+
+        # Construct iterator with sentinel value of None (for termination)
+        jobIterator = iter(self.JobQueue.get, None)
+
+        for jobArgs in jobIterator:
+            X, Mu, start, stop = jobArgs
+            self.printMsg("start=%d, stop=%d" % (start, stop))
+            msg = "X memory location: %d" % (getPtrForArray(X))
+            self.printMsg(msg)
+            SS = localStep_Vectorized(X, Mu, start=start, stop=stop)
+            self.ResultQueue.put(SS)
+            self.JobQueue.task_done()
+
+        self.printMsg("process CleanUp! pid=%d" % (os.getpid()))
+
+
 class TestKMeans(unittest.TestCase):
 
   def setUp(self, N=1000, D=25, K=10):
@@ -50,7 +90,10 @@ class TestKMeans(unittest.TestCase):
     self.Mu = rng.rand(K, D)
 
   def test_correctness_localStep(self):
-    ''' Verify that the local step works as expected
+    ''' Verify that the local step works as expected.
+
+    No parallelization here. Just verifying that we can split computation up
+    and still get the same answer.
     '''
     print ''
     N = self.X.shape[0]
@@ -67,20 +110,56 @@ class TestKMeans(unittest.TestCase):
     assert np.allclose(SSall.CountVec, SSbothparts.CountVec)
     assert np.allclose(SSall.DataStatVec, SSbothparts.DataStatVec)
 
+  def test_parallel_localStep(self, nWorkers=5):
+      """ Verify that we can execute local step across several processes
 
-  def localStep_SingleProcess(self, procID, processes):
-    N = self.X.shape[0]
-    batch_size = np.floor(N / float(processes))
-    start = procID * batch_size
-    if procID == processes - 1:
-      stop = N
-    else:
-      stop = (procID-1) * batch_size
-    return localStep_Vectorized(self.X, self.Mu, start=start, stop=stop)
+      Each process does the following:
+      * grab its chunk of data from a shared jobQueue
+      * performs computations on this chunk
+      * load the resulting sufficient statistics object into resultsQueue      
+      """
+      print ''
 
-  def test_parallel_localStep(self, processes=2):
+      # Create a JobQ (to hold tasks to be done)
+      # and a ResultsQ (to hold results of completed tasks)
+      manager = multiprocessing.Manager()
+      JobQ = manager.Queue()
+      ResultQ = manager.Queue()
 
-    pool = Pool(processes=processes)
-    output = pool.map(self.localStep_SingleProcess, range(processes))
-    print output
+      # Launch desired number of worker processes
+      for uid in range(nWorkers):
+          Worker(uid, JobQ, ResultQ).start()
+
+      # Create several tasks (one per worker) and add to job queue
+      N = self.X.shape[0]
+      batch_size = np.floor(N / float(nWorkers))
+      for workerID in range(nWorkers):
+          start = workerID * batch_size
+          stop = (workerID + 1) * batch_size
+          if workerID == nWorkers - 1:
+              stop = N
+          JobQ.put((self.X, self.Mu, start, stop))
+          # TODO: provide shared memory version of X/Mu instead??
+          # JobQ.put((X_shared, Mu_shared, start, stop))
+
+      # Pause at this line until all jobs are marked complete.
+      JobQ.join()
+
+      # REDUCE STEP: Aggregate results across across all workers
+      SS = ResultQ.get()
+      while not ResultQ.empty():
+          SSchunk = ResultQ.get()
+          SS += SSchunk
+
+      # Baseline: compute desired answer in master process.
+      SSall = localStep_Vectorized(self.X, self.Mu)  
+
+      print "Parallel Answer: CountVec.sum() = ", SS.CountVec.sum()
+      print "   Naive Answer: CountVec.sum() = ", SSall.CountVec.sum()
+      assert np.allclose(SSall.CountVec, SS.CountVec)
+
+      # Shut down all the workers
+      for workerID in range(nWorkers):
+          # Handing off None to JobQ is shutdown signal
+          JobQ.put( None )
 
