18e8913938d4b2224de4610eb629ad9375070f54
Sonia Phene <soniaphene@gmail.com>
2015-03-05 12:05:55 -0500

    Adding the inferLocalParamsForBatch method that is parallelized

--- bnpy/allocmodel/topics/LocalStepManyDocs.py
+++ bnpy/allocmodel/topics/LocalStepManyDocs.py
@@ -6,6 +6,10 @@ from bnpy.util import NumericUtil
 import LocalStepLogger
 
 import LocalStepSingleDoc as SDL
+import multiprocessing
+from multiprocessing import Process
+import multiprocessing.sharedctypes
+import ctypes
 
 def calcLocalParams(Data, LP, aModel, 
                           methodLP='scratch',
@@ -68,7 +72,124 @@ def calcLocalParams(Data, LP, aModel,
   LP['Info'] = AI
   return LP
 
+#Method that I wrote
+#Fake fake the one I am altering
 def inferLocalParamsForBatch(Data, aModel, Lik,
+                   initDocTopicCount=None,
+                   **kwargs
+                  ):
+  nProcesses=7
+  Processes=[]
+
+  #TODO: change!!! This doesn't fix anything (with regards to nDoc)!!
+  try:
+    nDoc=Data.nDoc
+  except Exception:
+    nDoc=2000
+
+  #Note: for this method had to make all of these 1-d arrays
+  doc_range=Data.doc_range
+  N, K = Lik.shape
+  alpha=0.5
+  sumRespTilde = np.zeros(N) 
+  ## Initialize DocTopicCount and Prior
+  DocTopicCount =  np.zeros(nDoc*K)
+  Prior = np.zeros(nDoc*K)
+
+  if str(type(aModel)).count('HDP'):
+    alphaEbeta = aModel.alphaEbeta[:-1].copy()
+    alphaEbetaRem = aModel.alphaEbeta[-1] * 1.0 # to float
+  elif str(type(aModel)).count('Finite'):
+    alphaEbeta = alpha * np.ones(K)
+    alphaEbetaRem = None
+
+  AggInfo = dict()
+  AggInfoMaxDiff = np.zeros(nDoc) 
+  AggInfoIter = np.zeros(nDoc, dtype=np.int32)
+
+  DocTopicCount=multiprocessing.Array(ctypes.c_double,DocTopicCount, lock=False) 
+  Prior= multiprocessing.Array(ctypes.c_double,Prior,lock=False)  
+  sumRespTilde=multiprocessing.Array(ctypes.c_double,sumRespTilde,lock=False) 
+  AggInfoMaxDiff=multiprocessing.Array(ctypes.c_double,AggInfoMaxDiff,lock=False) 
+  AggInfoIter=multiprocessing.Array(ctypes.c_double,AggInfoIter,lock=False) 
+
+  if hasattr(Data, 'word_count'):
+    Datawordcount= Data.word_count
+  else:
+    Datawordcount=None
+
+  dx=nDoc/nProcesses
+  start=0
+  end=0
+  while end<=nDoc: 
+    end+=dx
+    p=Process(target=tasked,args=(start,min(end,nDoc),doc_range,Lik, DocTopicCount, Prior, sumRespTilde, AggInfoMaxDiff, AggInfoIter,K,alphaEbeta,initDocTopicCount,Datawordcount)) 
+    Processes.append(p)
+    p.start()
+    start+=dx
+  
+  for p in Processes:
+    p.join()
+
+  DocTopicCount=np.array(list(DocTopicCount))
+  DocTopicCount=np.reshape(DocTopicCount,(nDoc,K))
+  Prior=np.array(list(Prior))
+  Prior=np.reshape(Prior,(nDoc,K))
+  sumRespTilde=np.array(list(sumRespTilde))
+  AggInfo['maxDiff']=list(AggInfoMaxDiff)
+  AggInfo['iter']=list(AggInfoIter)
+  return DocTopicCount, Prior, sumRespTilde, AggInfo
+
+def tasked(begin,end,doc_range,Lik,DocTopicCount, Prior, sumRespTilde, AggInfoMaxDiff, AggInfoIter,  K,alphaEbeta,initDocTopicCount,Datawordcount,**kwargs):
+
+  for d in range(begin,end):
+    # if d%10==0:
+    #   print d
+
+    try:
+      initDTC_d = initDocTopicCount[d]
+    except Exception:
+      initDTC_d = None
+
+    start = doc_range[d]
+    stop  = doc_range[d+1]
+    Lik_d = Lik[start:stop].copy() # Local copy
+
+    if Datawordcount!=None:
+      wc_d = Datawordcount[start:stop].copy()
+    else:
+      wc_d = 1.0
+    alphaEbetaRem= None #only works for finite not others!
+
+    #TODO: thought it should be -1 on second param but worked without!!
+    DocTopicCount[d*K:d*K+K], Prior[d*K:d*K+K], sumR_d, Info = SDL.inferLocal_SingleDoc_Dir(
+                                        wc_d, Lik_d, 
+                                        alphaEbeta, alphaEbetaRem,
+                                        initDTC_d,
+                                        **kwargs)
+
+    sumRespTilde[int(start):int(stop)] = sumR_d
+
+    ## Fill in AggInfo fields
+
+    AggInfoMaxDiff[d] = Info['maxDiff']
+    AggInfoIter[d] = Info['iter']
+
+    #Had to comment out the below ones, since AggInfo isn't a dictionary
+
+    #TODO: it looks as though ELBOtrace is sequential in that it is overwritten by later docs (this would not work otherwise)
+    # if 'ELBOtrace' in Info:
+    #   AggInfo['ELBOtrace'] = Info['ELBOtrace']
+    # if 'nAccept' in Info:
+    #   if 'nRestartsAccepted' not in AggInfo:
+    #     AggInfo['nRestartsAccepted'] = 0
+    #     AggInfo['nRestartsTried'] = 0
+    #   AggInfo['nRestartsAccepted'] += Info['nAccept']
+    #   AggInfo['nRestartsTried'] += Info['nTrial']
+
+
+#Below is the real method
+def inferLocalParamsForBatchReal(Data, aModel, Lik,
                    initDocTopicCount=None,
                    **kwargs
                   ):
