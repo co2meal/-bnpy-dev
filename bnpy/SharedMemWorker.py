c470d8dec4f27709284f6b3daaa86b22d7e0fc94
Sonia Phene <soniaphene@gmail.com>
2015-04-01 11:36:49 -0400

    adding the shared memory worker class

--- bnpy/SharedMemWorker.py
+++ bnpy/SharedMemWorker.py
@@ -47,18 +47,24 @@ import bnpy
 #give it a start and stop, could hand off things related to alloc model like alpha or beta vector
 
 
-
 class SharedMemWorker(multiprocessing.Process):
     """ Single "worker" process that processes tasks delivered via queues
     """
     def __init__(self, uid, JobQueue, ResultQueue, 
                  Data=None,
+                 arrArgs=None,
+                 LPkwargs=None,
                  verbose=0):
-        super(type(self), self).__init__() # Required super constructor call
+        super(Worker, self).__init__()
         self.uid = uid
+        self.Data = Data
+        if LPkwargs is None:
+            LPkwargs = dict()
+        self.LPkwargs = LPkwargs
+        self.arrArgs = arrArgs #TODO: ask Mike about this unpacking!
+
         self.JobQueue = JobQueue
         self.ResultQueue = ResultQueue
-        self.Data=Data
         self.verbose = verbose
 
     def printMsg(self, msg):
@@ -73,28 +79,21 @@ class SharedMemWorker(multiprocessing.Process):
         jobIterator = iter(self.JobQueue.get, None)
 
         for jobArgs in jobIterator:
-            start, stop, LP_part = jobArgs #TODO: **kwargs!!
-            if start is not None:
-                self.printMsg("start=%d, stop=%d" % (start, stop))
+            start, stop = jobArgs
+            sliceArgs = dict(cslice=(start, stop))
+            kwargs.update(sliceArgs)
 
-            msg = "X memory location: %d" % (getPtrForArray(self.Data))
-            self.printMsg(msg)
+            #TODO insert the static methods here
+            #TODO: could take in array arguments
+            LP = obsModelCalcLocalParams(self.Data, dict(), **kwargs)
+            LP = allocModelCalcLocalParams(self.Data, self.LPKwargs, **kwargs)
 
-            # LP_chunk=LP_resp[start:stop]
-            # LP_small=dict()
-            # LP_small['resp']=LP_chunk
-            # LP_small=self.allocModel.calc_local_params(self.Data, LP_small)
+            SS = allocModelGetGlobalSuffStats(self.Data, self.LPKwargs, **sliceArgs)
+            SS = obsModelGetGlobalSuffStats(self.Data, SS, LP, **sliceArgs)
 
-            #TODO: here we run into the problem of how to return this with the original shape, 
-            #if we are calling this with a smaller version
-
-            LP_resp=[[0 for i in range(len(LP_resp[0]))] for j in range(len(LP_resp))]#makes it entirely zeros and same size
-            LP_resp[start:stop]=LP_small['resp']
-
-            self.ResultQueue.put(LP_resp)
+            self.ResultQueue.put(SS)
             self.JobQueue.task_done()
 
         # Clean up
         self.printMsg("process CleanUp! pid=%d" % (os.getpid()))
-
 
