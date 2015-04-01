842b24a724b26ddc55dc766516b43e3b3d1914d0
Mike Hughes <mike@michaelchughes.com>
2015-04-01 07:44:48 -0400

    ENH polished TestFiniteTopicModel.py as executable, so its easy to run as script and understand speed gains from parallelization

--- tests/parallel/topics/TestFiniteTopicModel.py
+++ tests/parallel/topics/TestFiniteTopicModel.py
@@ -1,3 +1,42 @@
+"""
+Collection of functions and classes for testing a naive version 
+of parallel execution for topic model variational inference.
+
+Usage
+-----
+From a shell/terminal, use like a standard Python script.
+$ python TestFiniteTopicModel.py --N 200 --nDoc 500 --K 50 --nWorkers 2 
+
+For some keyword args (like N, nDoc, K, nWorkers),
+can use range syntax to easily compare performance as params change.
+$ python TestFiniteTopicModel.py --nWorkers 2-4 --nDoc 1000
+will repeat the test with 1000 documents for 2, 3, and 4 workers
+
+Keyword Args
+------------
+* N : int or range
+    number of words per document for toy data
+* nDoc : int or range 
+    total number of documents to generate
+* K : int or range
+    number of topics
+* nWorkers : int or range
+    number of worker processes for parallel execution
+* method : {'all', 'baseline', 'parallel', 'serial'}. Default = 'all'.
+    identifies which style of computation to perform
+* nRepeat : int
+    number of times to repeat each called method
+
+Methods
+-----------
+* runBenchmarkAcrossProblemSizes
+    Executable function that parses cmd-line args and runs benchmark tests.
+
+* calcLocalParamsAndSummarize
+    Execute local and summary step on slice of data. 
+    This is the function that we wish to call from each parallel worker.
+
+"""
 import os
 import multiprocessing
 from multiprocessing import sharedctypes
@@ -6,19 +45,91 @@ import numpy as np
 import unittest
 import ctypes
 import time
+import itertools
 
 import bnpy
 
-def calcLocalParamsAndSummarize(Data, hmodel, start=None, stop=None):
-    '''
+
+def runBenchmarkAcrossProblemSizes(TestClass):
+    """ Execute speed benchmark across several problem sizes.
+
+    This is main function executed by running this file as script.
+
+    Parameters
+    --------
+    TestClass : constructor for a TestCase instance
+        Must offer a run_speed_benchmark method.
+
+    Post Condition
+    --------
+    Speed tests are executed, and results are printed to std out.
+    """
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--nDoc', type=str, default='25')
+    parser.add_argument('--N', type=str, default='10000')
+    parser.add_argument('--K', type=str, default='10')
+    parser.add_argument('--nWorkers', type=str, default='2')
+    parser.add_argument('--vocab_size', type=int, default=100)
+    parser.add_argument('--nCoordAscentItersLP', type=int, default=100)
+    parser.add_argument('--convThrLP', type=float, default=0.001)
+    parser.add_argument('--method', type=str, default='all')
+    parser.add_argument('--nRepeat', type=int, default=1)
+    parser.add_argument('--verbose', type=int, default=0)
+    args = parser.parse_args()
+    
+    NKDiterator = itertools.product(
+        rangeFromString(args.N),
+        rangeFromString(args.K),
+        rangeFromString(args.nDoc),
+        rangeFromString(args.nWorkers))
+
+    kwargs = dict(**args.__dict__)
+
+    print "Speed Test."
+    print "    OMP_NUM_THREADS=%s" % (os.environ['OMP_NUM_THREADS'])
+    print "    vocab_size=%d" % (args.vocab_size)
+    print "    nCoordAscentItersLP=%d" % (args.nCoordAscentItersLP)
+    print "    convThrLP=%.2e" % (args.convThrLP)
+
+    for (N, K, nDoc, nWorkers) in NKDiterator:
+        print '========================= nDoc %d  N %d  K=%d | nWorkers %d' \
+            % (nDoc, N, K, nWorkers)
+        kwargs['N'] = N
+        kwargs['K'] = K
+        kwargs['nDoc'] = nDoc
+        kwargs['nWorkers'] = nWorkers
+
+        # Create test instance with desired keyword args.
+        # Required first arg is string name of test we'll execute
+        myTest = TestClass('run_speed_benchmark', **kwargs) 
+        myTest.setUp()
+        TimeInfo = myTest.run_speed_benchmark(method=args.method,
+                                              nRepeat=args.nRepeat)
+        myTest.tearDown() # closes all processes
+
+
+def calcLocalParamsAndSummarize(Data, hmodel, start=None, stop=None, **kwargs):
+    ''' Execute local step and summary step on slice of data.
+
+    Args
+    ----
+    Data : bnpy Data object
+    hmodel : bnpy HModel
+    start : int or None
+        id of data item that starts desired slice
+    stop : int or None
+        id of data item that ends desired slice
+
     Returns
     -----------
     SS : bnpy SuffStatBag
     '''
-    print '>>', start, stop
     sliceArgs = dict(cslice=(start, stop))
-    LP = hmodel.obsModel.calc_local_params(Data, dict(), **sliceArgs)
-    LP = hmodel.allocModel.calc_local_params(Data, LP, **sliceArgs)
+    kwargs.update(sliceArgs)
+    LP = hmodel.obsModel.calc_local_params(Data, dict(), **kwargs)
+    LP = hmodel.allocModel.calc_local_params(Data, LP, **kwargs)
 
     SS = hmodel.allocModel.get_global_suff_stats(Data, LP, **sliceArgs)
     SS = hmodel.obsModel.get_global_suff_stats(Data, SS, LP, **sliceArgs)
@@ -46,11 +157,15 @@ class Worker(multiprocessing.Process):
     def __init__(self, uid, JobQueue, ResultQueue, 
                  Data=None,
                  hmodel=None,
+                 LPkwargs=None,
                  verbose=0):
         super(Worker, self).__init__()
         self.uid = uid
         self.Data = Data
         self.hmodel = hmodel
+        if LPkwargs is None:
+            LPkwargs = dict()
+        self.LPkwargs = LPkwargs
 
         self.JobQueue = JobQueue
         self.ResultQueue = ResultQueue
@@ -71,7 +186,8 @@ class Worker(multiprocessing.Process):
             start, stop = jobArgs
 
             SS = calcLocalParamsAndSummarize(
-                self.Data, self.hmodel, start=start, stop=stop)
+                self.Data, self.hmodel, 
+                start=start, stop=stop, **self.LPkwargs)
             self.ResultQueue.put(SS)
             self.JobQueue.task_done()
 
@@ -84,17 +200,28 @@ class Test(unittest.TestCase):
     def shortDescription(self):
         return None
 
-    def setUp(self, N=1000, nDoc=50, K=10, vocab_size=100,
-              seed=0, nWorkers=2, 
-              verbose=1):
-        ''' 
+
+    def __init__(self, testname, seed=0, vocab_size=100,
+                nCoordAscentItersLP=100, convThrLP=0.01,
+                N=1000, nDoc=25, K=10, nWorkers=1, verbose=1,
+                **kwargs):
+        ''' Create a new test harness for parallel topic model inference.
+
+        Post Condition Attributes
+        --------------
+        Data : bnpy DataObj dataset
+        hmodel : bnpy HModel
         '''
+        super(type(self),self).__init__(testname)
+        self.nWorkers = nWorkers
+        self.verbose = verbose
         self.N = N
-        self.nDoc = nDoc
         self.K = K
+        self.nDoc = nDoc
+        self.LPkwargs = dict(nCoordAscentItersLP=nCoordAscentItersLP,
+                             convThrLP=convThrLP)
 
         PRNG = np.random.RandomState(seed)
-
         topics = PRNG.gamma(1.0, 1.0, size=(K, vocab_size))
         np.maximum(topics, 1e-30, out=topics)
         topics /= topics.sum(axis=1)[:,np.newaxis]
@@ -108,13 +235,15 @@ class Test(unittest.TestCase):
              dict(alpha=0.1, gamma=5),
              dict(lam=0.1), 
              self.Data)
-
         self.hmodel.init_global_params(self.Data, initname='randexamples', K=K)
 
+
+    def setUp(self, **kwargs):
+        ''' Launch pool of worker processes, with queues to communicate with.
+        '''
         # Create a JobQ (to hold tasks to be done)
         # and a ResultsQ (to hold results of completed tasks)
         manager = multiprocessing.Manager()
-        self.nWorkers = nWorkers
         self.JobQ = manager.Queue()
         self.ResultQ = manager.Queue()
 
@@ -125,7 +254,8 @@ class Test(unittest.TestCase):
             Worker(uid, self.JobQ, self.ResultQ, 
                    Data=self.Data,
                    hmodel=self.hmodel,
-                   verbose=verbose).start()
+                   LPkwargs=self.LPkwargs,
+                   verbose=self.verbose).start()
 
     def tearDown(self):
         """ Shut down all the workers.
@@ -142,7 +272,8 @@ class Test(unittest.TestCase):
     def run_baseline(self):
         """ Execute on entire matrix (no slices) in master process.
         """        
-        SSall = calcLocalParamsAndSummarize(self.Data, self.hmodel)
+        SSall = calcLocalParamsAndSummarize(self.Data, self.hmodel,
+                                            **self.LPkwargs)
         return SSall
 
     def run_serial(self):
@@ -151,7 +282,8 @@ class Test(unittest.TestCase):
         SSagg = None
         for start, stop in sliceGenerator(self.nDoc, self.nWorkers):
             SSslice = calcLocalParamsAndSummarize(
-                self.Data, self.hmodel, start, stop)
+                self.Data, self.hmodel, start, stop,
+                **self.LPkwargs)
             if start == 0:
                 SSagg = SSslice
             else:
@@ -177,27 +309,6 @@ class Test(unittest.TestCase):
             SS += SSchunk
         return SS
 
-    def run_with_timer(self, funcToCall, nRepeat=3):
-        starttime = time.time()
-        for r in xrange(nRepeat):
-            getattr(self, funcToCall)()
-        return (time.time() - starttime) / nRepeat
-
-    def run_all_with_timer(self, nRepeat=3):
-
-        serial_time = self.run_with_timer('run_serial')
-        parallel_time = self.run_with_timer('run_parallel')
-        base_time = self.run_with_timer('run_baseline')
-
-        return dict(
-            base_time=base_time,
-            base_speedup=1.0,
-            serial_time=serial_time,
-            serial_speedup=base_time/serial_time,
-            parallel_time=parallel_time,
-            parallel_speedup=base_time/parallel_time,
-            )
-
     def test_correctness_serial(self):
         ''' Verify that the local step works as expected.
 
@@ -220,7 +331,6 @@ class Test(unittest.TestCase):
         * load the resulting suff statistics object into resultsQueue      
         """
         print ''
-
         SSparallel = self.run_parallel()
         SSbase = self.run_baseline()
         allcloseSS(SSparallel, SSbase)
@@ -231,14 +341,90 @@ class Test(unittest.TestCase):
         """
         print ''
         Results = self.run_all_with_timer(nRepeat=nRepeat)
+        assert True
+    
+    def run_speed_benchmark(self, method='all', nRepeat=3):
+        """ Compare speed of different algorithms.
+        """
+        if method == 'all':
+            Results = self.run_all_with_timer(nRepeat=nRepeat)
+        elif method == 'parallel':
+            ptime = self.run_with_timer('run_parallel', nRepeat=nRepeat)
+            Results = dict(parallel_time=ptime)
+        elif method == 'serial':
+            ptime = self.run_with_timer('run_serial', nRepeat=nRepeat)
+            Results = dict(serial_time=ptime)    
+        else:
+            ptime = self.run_with_timer('run_baseline', nRepeat=nRepeat)
+            Results = dict(base_time=ptime)
+            
 
         for key in ['base_time', 'serial_time', 'parallel_time']:
-            print "%18s | %8.3f sec | %8.3f speedup" % (
-                key, 
-                Results[key], 
-                Results[key.replace('time', 'speedup')],
-                )
-    
+            if key in Results:
+                try:
+                    speedupval = Results[key.replace('time', 'speedup')]
+                    speedupmsg = "| %8.3f speedup" % (speedupval)
+                except KeyError:
+                    speedupmsg = ""
+                print "%18s | %8.3f sec %s" % (
+                    key, 
+                    Results[key], 
+                    speedupmsg
+                    )
+        return Results
+
+    def run_with_timer(self, funcToCall, nRepeat=3):
+        """ Timing experiment specified by funcToCall.
+        """
+        starttime = time.time()
+        for r in xrange(nRepeat):
+            getattr(self, funcToCall)()
+        return (time.time() - starttime) / nRepeat
+
+    def run_all_with_timer(self, nRepeat=3):
+        """ Timing experiments with baseline, serial, and parallel versions.
+        """
+        serial_time = self.run_with_timer('run_serial', nRepeat)
+        parallel_time = self.run_with_timer('run_parallel', nRepeat)
+        base_time = self.run_with_timer('run_baseline', nRepeat)
+
+        return dict(
+            base_time=base_time,
+            base_speedup=1.0,
+            serial_time=serial_time,
+            serial_speedup=base_time/serial_time,
+            parallel_time=parallel_time,
+            parallel_speedup=base_time/parallel_time,
+            )
+
+
+
+def rangeFromString(commaString):
+    """ Convert a comma string like "1,5-7" into a list [1,5,6,7]
+
+    Returns
+    --------
+    myList : list of integers
+
+    Reference
+    -------
+    http://stackoverflow.com/questions/6405208/\
+    how-to-convert-numeric-string-ranges-to-a-list-in-python
+    """
+    listOfLists = [rangeFromHyphen(r) for r in commaString.split(',')]
+    flatList = itertools.chain(*listOfLists)
+    return flatList
+
+def rangeFromHyphen(hyphenString):
+    """ Convert a hyphen string like "5-7" into a list [5,6,7]
+
+    Returns
+    --------
+    myList : list of integers
+    """
+    x = [int(x) for x in hyphenString.split('-')]
+    return range(x[0], x[-1]+1)
+
 
 def allcloseSS(SS1, SS2):
     """ Verify that two suff stat bags have indistinguishable data.
@@ -258,3 +444,7 @@ def allcloseSS(SS1, SS2):
             print arr1[:2, :3]
             print arr2[:2, :3]
         assert np.allclose(arr1, arr2)
+
+
+if __name__ == "__main__":
+    runBenchmarkAcrossProblemSizes(Test)
