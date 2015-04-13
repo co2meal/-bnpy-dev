fef714364aee7f866c9384f4ba3e48c254d1ce2d
Sonia Phene <sphene@cs.brown.edu>
2015-04-13 16:10:45 -0400

    The scripts for Toy Bars

--- portions.py
+++ portions.py
@@ -1,14 +1,15 @@
+#!/contrib/projects/EnthoughtPython/epd64/bin/python
 import time
 import bnpy
 
 
 for i in range(1,13):
-	for k in range(1,5):
+	for k in range(8,16):
 		f=open('overalltimes'+str(k)+'.txt','a') 
 		start=time.time()
-		bnpy.run('BarsK10V900','FiniteTopicModel','Mult','pVB',nDocTotal=15000,nWordsPerDoc=200,K=50,nLap=1,nCoordAscentItersLP=100,convThrLP=.000001,initname='randomlikewang',lam=.1,alpha=.5,nWorkers=i)
+		bnpy.run('BarsK10V900','FiniteTopicModel','Mult','pVB',nDocTotal=15000,nWordsPerDoc=200,K=50,nLap=1,nCoordAscentItersLP=100,convThrLP=.000001,initname='randomlikewang',lam=.1,alpha=.5,nWorkers=k)
 		duration=time.time()-start
-		f.write("Overall time was"+str(duration)+'\n')
+		f.write(str(duration)+'\n')
 
 
 #NOTE: overalltimes and overallportiontimes are with 1 lap only!!
---
+++ realportions.sh
@@ -0,0 +1,8 @@
+#!/usr/local/bin/bash
+
+export PATH="/contrib/projects/EnthoughtPython/epd64/bin/:$PATH" 
+export PYTHONPATH="/home/sphene/bnpy/bnpy-dev"
+export BNPYOUTDIR="/home/sphene/bnpy/out"
+
+
+./portions.py
