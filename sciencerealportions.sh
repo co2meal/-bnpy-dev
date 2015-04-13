c1b623c9fdd131896c499251e612cb9fdb241062
Sonia Phene <sphene@cs.brown.edu>
2015-04-13 13:21:32 -0400

    Adding the scripts that I used to run experiments. The bash script merely calls the python script and exports environmental variables

---
+++ sciencerealportions.sh
@@ -0,0 +1,9 @@
+#!/usr/local/bin/bash
+
+export PATH="/contrib/projects/EnthoughtPython/epd64/bin/:$PATH" 
+export PYTHONPATH="/home/sphene/bnpy/bnpy-dev"
+export BNPYOUTDIR="/home/sphene/bnpy/out"
+export BNPYDATADIR="/home/sphene/bnpy/data/science/"
+
+
+./scienceportions.py
