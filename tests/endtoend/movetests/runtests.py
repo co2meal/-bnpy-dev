53c901590641a29b32581da2585980c212aeb9ab
Mike Hughes <mike@michaelchughes.com>
2015-04-20 15:45:09 -0400

    ENH default option for deleteNontargetStrategy is now 'forget'. endtoend tests now look at deletes as well as merges

--- tests/endtoend/movetests/runtests.py
+++ tests/endtoend/movetests/runtests.py
@@ -5,5 +5,5 @@ testroot = os.path.sep.join(os.path.abspath(__file__).split(os.path.sep)[:-1])
 if not testroot.endswith(os.path.sep):
     testroot = testroot + os.path.sep
 
-CMD = "%s -v --nocapture --attr=fast" % (testroot)
+CMD = "%s -v --nocapture " % (testroot)
 result = nose.run(argv=CMD.split())
