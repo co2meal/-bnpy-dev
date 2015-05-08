FIX HDPHMM merge construction when hmmKappa > 0 had a bug where candidate used for merge gap calculation did not add hmmKappa mass to self transitions. With this change, merges work much better now.

bnpy/allocmodel/hmm/HDPHMMUtil.py
@@ -123,7 +123,7 @@ def L_top(rho=None, omega=None, alpha=None,
     if kappa > 0:
         coefU = K + 1.0 - eta1
         coef1mU = K * OptimizerRhoOmega.kvec(K) + 1.0 + gamma - eta0
-        sumEBeta = np.sum(StickBreakUtil.rho2beta(rho, returnSize='K'))
+        sumEBeta = np.sum(rho2beta(rho, returnSize='K'))
         tBeta = sumEBeta * (np.log(alpha + kappa) - np.log(kappa))
         tKappa = K * (np.log(kappa) - np.log(alpha + kappa))
     else:

bnpy/allocmodel/hmm/HMMUtil.py
@@ -57,7 +57,13 @@ def calcLocalParams(Data, LP,
         mPairIDs = np.zeros((0, 2))
         M = 0
     else:
-        M = mPairIDs.shape[0]
+        if len(mPairIDs) == 0:
+            mPairIDs = np.zeros((0, 2))
+            M = 0
+        else:
+            mPairIDs = as2D(mPairIDs)
+            M = mPairIDs.shape[0]
+    assert mPairIDs.shape[1] == 2
 
     if limitMemoryLP:
         # Track sufficient statistics directly at each sequence.