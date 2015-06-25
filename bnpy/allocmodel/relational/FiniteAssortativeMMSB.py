ENH FiniteAssortativeMMSB now does correct entropy calculation, as written in Will's thesis. Also, RelationalViz has some fixes to plot both Assortative and nonAssortative results

bnpy/allocmodel/relational/FiniteAssortativeMMSB.py
@@ -86,8 +86,7 @@ class FiniteAssortativeMMSB(FiniteMMSB):
             LP['E_log_soft_ev']
         np.exp(resp, out=resp)
 
-        expElogPi = ElogPi
-        np.exp(ElogPi, out=expElogPi)
+        expElogPi = np.exp(ElogPi)
 
         # sumPi_fg : 1D array, size nEdges
         #    sumPi_fg[e] = \sum_k \pi[s,k] \pi[t,k] for edge e=(s,t)
@@ -137,12 +136,38 @@ class FiniteAssortativeMMSB(FiniteMMSB):
         resp_bg = 1.0 - resp.sum(axis=1)
         LP['Ldata_bg'] = np.inner(resp_bg, logepsEvVec)
 
-        LP['Lentropy_normConst'] = np.sum(respNormConst)
+        LP['Lentropy_normConst'] = np.sum(np.log(respNormConst))
         LP['Lentropy_lik_fg'] = -1 * np.sum(
             LP['resp']*LP['E_log_soft_ev'], axis=0)
         LP['Lentropy_prior'] = -1 * np.sum(
             LP['NodeStateCount'] * ElogPi, axis=0)
         LP['Lentropy_lik_bg'] = -1 * LP['Ldata_bg']
+        return LP
+
+
+    def initLPFromResp(self, Data, LP):
+        ''' Initialize local parameters given LP dict with resp field.
+        '''
+        K = LP['resp'].shape[-1]
+        if LP['resp'].ndim == 2:
+            resp = LP['resp']
+            raise NotImplementedError("TODO")
+        else:
+            resp = np.zeros((Data.nEdges, K))
+            for k in xrange(K):
+                resp[:,k] = LP['resp'][:, k, k]
+            srcresp_bg = LP['resp'].sum(axis=2) - resp
+            rcvresp_bg = LP['resp'].sum(axis=2) - resp
+
+        LP['resp'] = resp
+        NodeStateCount_bg = \
+            Data.getSparseSrcNodeMat() * srcresp_bg + \
+            Data.getSparseRcvNodeMat() * rcvresp_bg
+        # NodeStateCount_fg : 2D array, size nNodes x K
+        nodeMat = Data.getSparseSrcNodeMat() + Data.getSparseRcvNodeMat()
+        NodeStateCount_fg = nodeMat * LP['resp']
+        LP['NodeStateCount'] = NodeStateCount_bg + NodeStateCount_fg
+        LP['N_fg'] = NodeStateCount_fg.sum(axis=0)
         return LP
 
 
@@ -164,15 +189,17 @@ class FiniteAssortativeMMSB(FiniteMMSB):
 
         SS.setField('N', LP['N_fg'], dims=('K',))
         SS.setField('scaleFactor', Data.nEdges, dims=None)
-        SS.setELBOTerm('Ldata_bg', LP['Ldata_bg'], dims=None)
+
+        if 'Ldata_bg' in LP:
+            SS.setELBOTerm('Ldata_bg', LP['Ldata_bg'], dims=None)
 
         if doPrecompEntropy:
             Hresp = LP['Lentropy_prior'].sum() + \
                 LP['Lentropy_lik_fg'].sum() + \
                 LP['Lentropy_normConst'] + \
                 LP['Lentropy_lik_bg']
-            Hresp2 = self.L_entropy(LP)
-            SS.setELBOTerm('Hresp', Hresp2, dims=None)
+            # easy lower bound: Hresp_lb = self.L_entropy(LP)
+            SS.setELBOTerm('Hresp', Hresp, dims=None)
         return SS
 
  

bnpy/viz/RelationalViz.py
@@ -182,14 +182,29 @@ def drawGraphEdgePr(Data, Epi, Ew, curAx, fig, colors=None, labels=None,
     plt.ylim(0 - ygap, ymax + ygap)
 
 
-def plotSingleJob(dataset, jobname, taskids='1', lap='final',
+def plotSingleJob(dataName, jobname, taskids='1', lap='final',
                   showELBOInTitle=True, cmap='gray', title='', mixZs=False):
-    Datamod = imp.load_source(
-        dataset,
-        os.path.expandvars('$BNPYDATADIR/' + dataset + '.py'))
-    Data = Datamod.get_data()
+    ''' Visualize results of single run
+    '''
+
+    # Load data, with same dataset size prefs as specified at inference time.
     jobpath = os.path.join(os.path.expandvars('$BNPYOUTDIR'),
-                           Datamod.get_short_name(), jobname)
+                           dataName, jobname)
+    datasetPrefFile = os.path.join(
+        jobpath, taskids[0],
+        'args-DatasetPrefs.txt')
+    datasetPrefs = dict()
+    if os.path.exists(datasetPrefFile):
+        with open(datasetPrefFile, 'r') as f:
+            for line in f.readlines():
+                fields = line.strip().split(' ')
+                if len(fields) != 2:
+                    continue
+                datasetPrefs[fields[0]] = fields[1]
+    Datamod = imp.load_source(
+        dataName,
+        os.path.expandvars('$BNPYDATADIR/' + dataName + '.py'))
+    Data = Datamod.get_data(**datasetPrefs)
 
     if isinstance(taskids, str):
         taskids = BNPYArgParser.parse_task_ids(jobpath, taskids)
@@ -210,7 +225,7 @@ def plotSingleJob(dataset, jobname, taskids='1', lap='final',
 
     # Show the true adj mat and the estimated side-by-side
     ncols = len(taskids)+1
-    plt.subplots(nrows=1, ncols=ncols, figsize=(3, 3*ncols))
+    plt.subplots(nrows=1, ncols=ncols, figsize=(3*ncols, 3))
     plt.subplot(1, ncols, 1)
     plt.imshow(AdjMat, cmap='Greys', interpolation='nearest', vmin=0, vmax=1)
 
@@ -233,16 +248,13 @@ def plotSingleJob(dataset, jobname, taskids='1', lap='final',
             (hmodel.obsModel.Post.lam1 + hmodel.obsModel.Post.lam0)
         LP = hmodel.calc_local_params(Data)
         taskAdjMat = np.zeros((Data.nNodes, Data.nNodes, Data.dim))
-        for s in range(Data.nNodes):
-            for t in range(Data.nNodes):
-                if s == t:
-                    continue
-                eid = np.flatnonzero(
-                    np.logical_and(Data.edges[:,0] == s, 
-                                   Data.edges[:,1] == t))[0]
-                resp_st = LP['resp'][eid, :, :]
-                taskAdjMat[s,t] = np.sum(resp_st[:,:,np.newaxis] * Ew, axis=(0,1))
+        for eid, (s,t) in enumerate(Data.edges):
+            resp_st = LP['resp'][eid]
+            if resp_st.ndim == 2: # K x K
                 assert np.allclose(resp_st.sum(), 1.0)
+                taskAdjMat[s,t] = np.sum(resp_st[:,:,np.newaxis] * Ew, axis=(0,1))
+            else:
+                taskAdjMat[s,t] = np.sum(resp_st[:,np.newaxis] * Ew, axis=0)
 
         assert taskAdjMat.min() >= 0
         assert taskAdjMat.max() <= 1.0
@@ -254,7 +266,6 @@ def plotSingleJob(dataset, jobname, taskids='1', lap='final',
         plt.imshow(taskAdjMat,
                    cmap='Greys', interpolation='nearest', vmin=0, vmax=1)
         
-        print 'ERROR: ', np.sum(AdjMat - taskAdjMat)
         '''
         if showELBOInTitle:
             ELBOscores = np.loadtxt(os.path.join(path, 'evidence.txt'))
@@ -524,7 +535,7 @@ if __name__ == "__main__":
     parser.add_argument('--lap', default='final')
     args = parser.parse_args()
 
-    plotSingleJob(dataset=args.dataName,
+    plotSingleJob(dataName=args.dataName,
                   jobname=args.jobname,
                   taskids=args.taskids,
                   lap=args.lap)