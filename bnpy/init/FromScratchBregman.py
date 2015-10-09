99022c8be554c2e595c05c464803b624b3255f1c
michaelchughes <mike@michaelchughes.com>
2015-10-09 10:36:19 -0400

    FIX Init from K=1 now works with brgkmns

--- bnpy/init/FromScratchBregman.py
+++ bnpy/init/FromScratchBregman.py
@@ -277,7 +277,7 @@ def makeDataSubsetByThresholdResp(
 
         # Raise error if target dataset not big enough.
         Keff = np.minimum(K, rowsWithEnoughData.size)
-        if Keff <= 1:
+        if Keff <= 1 and K > 1:
             DebugInfo = dict(
                 targetAssemblyMsg=targetAssemblyMsg,
                 atomType=atomType,
@@ -336,7 +336,7 @@ def makeDataSubsetByThresholdResp(
 
             # Raise error if target dataset not big enough.
             Keff = np.minimum(K, chosenRespIDs.size)
-            if Keff <= 1:
+            if Keff <= 1 and K > 1:
                 DebugInfo = dict(
                     targetAssemblyMsg=targetAssemblyMsg,
                     atomType=atomType,
