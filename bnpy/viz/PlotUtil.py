691aec9df17033fdb9010806c036e07b0e45ed31
michaelchughes <mike@michaelchughes.com>
2015-10-14 09:06:31 -0400

    ENH Updated sanity checks for bregman

--- bnpy/viz/PlotUtil.py
+++ bnpy/viz/PlotUtil.py
@@ -1,4 +1,7 @@
 from matplotlib import pylab
+import matplotlib.axis, matplotlib.scale 
+from matplotlib.ticker import \
+    MaxNLocator, ScalarFormatter, NullLocator, NullFormatter
 
 ExportInfo = dict(
     doExport=False,
@@ -6,7 +9,6 @@ ExportInfo = dict(
     W_in=4,
     H_in=4,
 )
-
 
 def ConfigPylabDefaults(pylab, **kwargs):
     rcParams = pylab.rcParams
@@ -25,3 +27,20 @@ def ConfigPylabDefaults(pylab, **kwargs):
     rcParams['figure.subplot.top'] = 0.95
     rcParams['savefig.dpi'] = ExportInfo['dpi']
     rcParams.update(kwargs)
+
+def set_my_locators_and_formatters(self, axis):
+    # choose the default locator and additional parameters
+    if isinstance(axis, matplotlib.axis.XAxis):
+        axis.set_major_locator(MaxNLocator(prune='lower'))
+    elif isinstance(axis, matplotlib.axis.YAxis):
+        axis.set_major_locator(MaxNLocator())
+    # copy & paste from the original method
+    axis.set_major_formatter(ScalarFormatter())
+    axis.set_minor_locator(NullLocator())
+    axis.set_minor_formatter(NullFormatter())
+
+# Set defaults for nbins in xticks and yticks
+MaxNLocator.default_params['nbins']=5
+matplotlib.scale.LinearScale.set_default_locators_and_formatters = \
+    set_my_locators_and_formatters
+
--- tests/obsmodel/TestExpFamBregman_Gauss.py
+++ tests/obsmodel/TestExpFamBregman_Gauss.py
@@ -9,41 +9,57 @@ PlotUtil.ConfigPylabDefaults(
     **{'figure.subplot.left':0.23,
        'figure.subplot.bottom':0.23})
 
-def make_phi_grid(ngrid=1000, min_phi=-20, max_phi=-1e-10):
-    phi_grid = np.linspace(min_phi, max_phi, ngrid)
-    return phi_grid
+def make_phi1_grid(ngrid=1000, min_phi=-20, max_phi=-1e-10):
+    phi1_grid = np.linspace(min_phi, max_phi, ngrid)
+    return phi1_grid
 
-def make_mu_grid(ngrid=1000, min_mu=1e-10, max_mu=20):
-    mu_grid = np.linspace(min_mu, max_mu, ngrid)
-    return mu_grid
+def make_phi2_grid(ngrid=1000, min_phi2=-20, max_phi2=20):
+    phi2_grid = np.linspace(min_phi2, max_phi2, ngrid)
+    return phi2_grid
 
-def phi2mu(phi_grid):
-    mu_grid = -0.5 * 1.0/phi_grid
-    return mu_grid
+def make_mu1_grid(ngrid=1000, min_mu=1e-10, max_mu=30):
+    mu1_grid = np.linspace(min_mu, max_mu, ngrid)
+    return mu1_grid
 
-def mu2phi(mu_grid):
-    phi_grid = -0.5 * 1.0/mu_grid
-    return phi_grid
+def make_mu2_grid(ngrid=1000, min_mu=-30, max_mu=30):
+    mu2_grid = np.linspace(min_mu, max_mu, ngrid)
+    return mu2_grid
 
-def c_Phi(phi_grid):
-    c_grid = - 0.5 * np.log(- phi_grid)
+def phi2mu(phi1_grid, phi2_grid):
+    inv_phi1 = 1.0 / phi2_grid
+    mu1_grid = -0.5 * inv_phi1 + \
+        0.25 * np.square(inv_phi1) * np.square(phi2_grid)
+    mu2_grid = -0.5 * inv_phi1 * phi2_grid
+    return mu1_grid, mu2_grid
+
+def mu2phi(mu1_grid, mu2grid):
+    inv_mu = 1.0/(mu1_grid - np.square(mu2_grid))
+    phi1_grid = -0.5 * inv_mu
+    phi2_grid = mu2_grid * inv_mu
+    return phi1_grid, phi2_grid
+
+def c_Phi(phi1_grid, phi2_grid):
+    c_grid = - 0.5 * np.log(- phi1_grid) - 0.25 * np.square(phi2_grid)/phi1_grid
     return c_grid
 
-def c_Mu(mu_grid):
-    c_grid = - 0.5 * np.log(mu_grid)
+def c_Mu(mu1_grid, mu2_grid):
+    c_grid = - 0.5 * np.log(mu1_grid - np.square(mu2_grid))
     return c_grid
 
-def E_phi(nu=0, tau=0):
+def E_phi1(nu=0, tau=0):
     return - (0.5 * nu + 1.0) / tau
 
-def bregmanDiv(mu_grid, mu):
+def bregmanDiv(mu1_grid, mu2_grid, mu1, mu2):
     '''
 
     Returns
     -------
     div : 1D array, size ngrid
     '''
-    div = c_Mu(mu_grid) - c_Mu(mu) - (mu_grid - mu) * mu2phi(mu)
+    phi1, phi2 = mu2phi(mu)
+    div = c_Mu(mu1_grid, mu2_grid) - c_Mu(mu1, mu2) - \
+        (mu1_grid - mu1) * phi1 - \
+        (mu2_grid - mu2) * phi2
     return div
 
 def c_Prior(nu=0, tau=0):
@@ -58,7 +74,7 @@ def logJacobian_mu(mu_grid):
         J[n] = log Jacobian(mu[n], mu2phi)
         Jacobian(mu, mu2phi) = d/dmu phi(mu)
     '''
-    return np.log(0.5) - 2 * np.log(mu_grid)
+    pass
 
 def checkFacts_pdf_Phi(
         nu=0, tau=0, phi_grid=None, **kwargs):
@@ -133,21 +149,37 @@ def checkFacts_pdf_Mu(nu=0, tau=0, mu_grid=None, **kwargs):
     print "       E[phi]=% 7.3f   should be % 7.3f" % (E_phi_numeric, E_phi_formula)
 
 def makePlot_cumulant_Phi(
-        phi_grid=None, **kwargs):
-    if phi_grid is None:
-        phi_grid = make_phi_grid(**kwargs)
-    c_grid = c_Phi(phi_grid)
-    pylab.plot(phi_grid, c_grid, 'k-')
-    pylab.xlabel('phi')
+        phi1=None, phi2=None, **kwargs):
+    if phi1 is None:
+        phi1 = make_phi1_grid(**kwargs)
+        grid = phi1
+        label = 'phi2 = %7.3f' % (phi2)
+        xlabel = 'phi_1'
+    if phi2 is None:
+        phi2 = make_phi2_grid(**kwargs)
+        grid = phi2
+        label = 'phi1 = %7.3f' % (phi1)
+        xlabel = 'phi_2'
+    c_grid = c_Phi(phi1, phi2)
+    pylab.plot(grid, c_grid, '-', label=label)
+    pylab.xlabel(xlabel)
     pylab.ylabel('c(phi)')
 
 def makePlot_cumulant_Mu(
-        mu_grid=None, **kwargs):
-    if mu_grid is None:
-        mu_grid = make_mu_grid(**kwargs)
-    c_grid = c_Mu(mu_grid)
-    pylab.plot(mu_grid, c_grid, 'r-')
-    pylab.xlabel('mu')
+        mu1=None, mu2=None, **kwargs):
+    if mu1 is None:
+        mu1 = make_mu1_grid(**kwargs)
+        grid = mu1
+        label = 'mu2 = %7.3f' % (mu2)
+        xlabel = 'mu1'
+    if mu2 is None:
+        mu2 = make_mu2_grid(**kwargs)
+        grid = mu2
+        label = 'mu1 = %7.3f' % (mu1)
+        xlabel = 'mu2'
+    c_grid = c_Mu(mu1, mu2)
+    pylab.plot(grid, c_grid, '-', label=label)
+    pylab.xlabel(xlabel)
     pylab.ylabel('gamma(mu)')
 
 def makePlot_cumulant_Prior(
@@ -170,6 +202,22 @@ def makePlot_bregmanDiv_Mu(
     pylab.ylabel('Bregman div.  D(x, mu)')
 
 if __name__ == '__main__':
+
+    pylab.figure()
+    for mu2 in [-2.1, -1.1, 0, 1, 2, 4][::-1]:
+        makePlot_cumulant_Mu(mu2=mu2, min_mu=np.square(mu2)+1e-10, max_mu=100)
+    pylab.legend(loc='upper right', fontsize=13)
+    pylab.xlim([-2, 100])
+
+    '''
+    pylab.figure()
+    for phi2 in [-10.5, -5.5, 0, 5, 10, 20][::-1]:
+        makePlot_cumulant_Phi(phi2=phi2, min_phi=-30)
+    pylab.ylim([-5, 30])
+    pylab.xlim([-20, 0.5])
+    pylab.legend(loc='upper left', fontsize=13)
+
+
     nuRange = [1/2.0, 1, 2.0, 8, 32, 128]
     mu_Phi = 0.5 * 1.0/3.0
     print "mu(Mode[phi]): ", mu_Phi
@@ -184,7 +232,6 @@ if __name__ == '__main__':
     pylab.xticks([-12, -9, -6, -3, 0])
     pylab.savefig('ZMG_densityPhi.eps')
     
-    '''
     pylab.figure()
     for nu in nuRange[::-1]:
         tau = mu_Phi * nu
