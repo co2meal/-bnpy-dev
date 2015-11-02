FIX HDP Topics sign error in calcHrespForMergePairs, where we didn't multiply by neg. one when should have.

bnpy/allocmodel/topics/HDPTopicUtil.py
@@ -263,24 +263,25 @@ def calcHrespForMergePairs(resp, Data, mPairIDs, returnVec=1):
     '''
     if hasattr(Data, 'word_count'):
         if mPairIDs is None:
-            Hmat = calcRlogRdotv_allpairs(resp, Data.word_count)
+            Hmat = -1 * calcRlogRdotv_allpairs(resp, Data.word_count)
         else:
-            Hmat = calcRlogRdotv_specificpairs(resp, Data.word_count, mPairIDs)
+            Hmat = -1 * calcRlogRdotv_specificpairs(
+                resp, Data.word_count, mPairIDs)
     else:
         if mPairIDs is None:
-            Hmat = calcRlogR_allpairs(resp)
+            Hmat = -1 * calcRlogR_allpairs(resp)
         else:
-            Hmat = calcRlogR_specificpairs(resp, mPairIDs)
+            Hmat = -1 * calcRlogR_specificpairs(resp, mPairIDs)
     if returnVec:
         if Hmat.ndim == 2:
             Hvec = np.zeros(len(mPairIDs))
             for ii, (kA, kB) in enumerate(mPairIDs):
-                Hvec[ii] = -1 * Hmat[kA, kB]
+                Hvec[ii] = Hmat[kA, kB]
         else:
             Hvec = Hmat
         return Hvec
     else:
-        return -1 * Hmat
+        return Hmat
 
 
 def c_Beta(a1, a0):

bnpy/obsmodel/ZeroMeanGaussObsModel.py
@@ -741,10 +741,6 @@ class ZeroMeanGaussObsModel(AbstractObsModel):
         for k in xrange(K):
             chol_Mu_k = np.linalg.cholesky(Mu[k])
             logdet_Mu_k = 2.0 * np.sum(np.log(np.diag(chol_Mu_k)))
-            # xxTInvMu_k = np.linalg.solve(chol_Mu_k, X.T)
-            # xxTInvMu_k *= xxTInvMu_k
-            # tr_xxTInvMu_k = np.sum(xxTInvMu_k, axis=0)
-            # assert tr_xxTInvMu_k.shape == (N,)
 
             Div[:,k] = -0.5 * D - 0.5 * logdet_xxT + \
                 0.5 * logdet_Mu_k + \
@@ -752,9 +748,9 @@ class ZeroMeanGaussObsModel(AbstractObsModel):
 
         if W is not None:
             Div *= W[:,np.newaxis]
-        assert Div.min() > -1e-8
-        np.maximum(Div, 0, out=Div)
-        assert Div.min() >= 0
+        if smoothFrac > 0:
+            assert Div.min() > -1e-8
+            np.maximum(Div, 0, out=Div)
         return Div
 
     def calcBregDivFromPrior(self, Mu, smoothFrac=0.0):