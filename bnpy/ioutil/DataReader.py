142eeab219678303e7a76889307e96d9e2db81a7
michaelchughes <mike@michaelchughes.com>
2015-11-05 10:50:27 -0500

    ENH Added DataReader.py, which allows an easy function to load the training data specifically used for a saved task.

---
+++ bnpy/ioutil/DataReader.py
@@ -0,0 +1,84 @@
+import os
+import numpy as np
+
+def loadDataFromSavedTask(taskoutpath, **kwargs):
+    ''' Load data object used for training a specified saved run.
+
+    Args
+    ----
+    taskoutpath : full path to saved results of bnpy training run
+
+    Returns
+    -------
+    Dslice : bnpy Data object
+        used for training the specified task.
+
+    Example
+    -------
+    >>> import bnpy
+    >>> os.environ['BNPYOUTDIR'] = '/tmp/'
+    >>> hmodel, Info = bnpy.run(
+    ...     'AsteriskK8', 'FiniteMixtureModel', 'Gauss', 'VB',
+    ...     nLap=1, nObsTotal=144, K=10,
+    ...     doWriteStdOut=False)
+    >>> outputdir = Info['outputdir']
+    >>> Data2 = loadDataFromSavedTask(outputdir)
+    >>> print Data2.nObsTotal
+    144
+    >>> np.allclose(Info['Data'].X, Data2.X)
+    True
+    '''
+    dataName = getDataNameFromTaskpath(taskoutpath)
+    dataKwargs = loadDataKwargsFromDisk(taskoutpath, dataName)
+    datamod = __import__(dataName, fromlist=[])
+    Data = datamod.get_data(**dataKwargs)
+    return Data
+
+def loadDataKwargsFromDisk(taskoutpath, dataName=None):
+    ''' Load keyword options used to load specific dataset.
+
+    Returns
+    -------
+    dataKwargs : dict with options for loading dataset 
+    '''
+    if dataName is None:
+        dataName = getDataNameFromTaskpath(taskoutpath)
+    txtfilepath = os.path.join(taskoutpath, 'args-DatasetPrefs.txt')
+
+    dataKwargs = dict()
+    with open(txtfilepath, 'r') as f:
+        for line in f.readlines():
+            fields = line.strip().split(' ')
+            assert len(fields) == 2
+            try:
+                val = int(fields[1])
+            except Exception:
+                val = str(fields[1])
+            dataKwargs[fields[0]] = val
+    return dataKwargs
+
+def getDataNameFromTaskpath(taskoutpath):
+    ''' Extract dataset name from bnpy output filepath.
+
+    Returns
+    -------
+    dataName : string identifier of a dataset
+
+    Examples
+    --------
+    >>> os.environ['BNPYOUTDIR'] = '/tmp/'
+    >>> taskoutpath = '/tmp/MyDataName/myjobname/1/'
+    >>> dataName = getDataNameFromTaskpath(taskoutpath)
+    >>> print dataName
+    MyDataName
+    '''
+    # Make it a proper absolute path
+    taskoutpath = os.path.abspath(taskoutpath)
+    # Extract the dataset name from taskoutpath
+    strippedpath = taskoutpath.replace(os.environ['BNPYOUTDIR'], '')
+    if strippedpath.startswith(os.path.sep):
+        strippedpath = strippedpath[1:]
+    # The very next segment must be the data name
+    dataName = strippedpath[:strippedpath.index(os.path.sep)]
+
+    return dataName
