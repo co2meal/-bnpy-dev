FIX faster, less-memory-hungry cython code for entropy calculation needed some tweaks to play well with proposal moves

bnpy/allocmodel/mix/DPMixtureModel.py
@@ -364,7 +364,7 @@ class DPMixtureModel(AllocModel):
             kA = SSa.uid2k(uidA)
             kB = SSb.uid2k(uidB)
             respAB = LPa['resp'][:, kA] + LPb['resp'][:, kB]
-            m_Hresp[m] = -1 * NumericUtil.calcRlogR(respAB[:,np.newaxis])
+            m_Hresp[m] = -1 * NumericUtil.calcRlogR(respAB)
         assert m_Hresp.min() > -1e-9
         return dict(Hresp=m_Hresp)
 

bnpy/allocmodel/mix/DPMixtureRestrictedLocalStep.py
@@ -75,7 +75,7 @@ def summarizeRestrictedLocalStep_DPMixtureModel(
     else:
         target_resp = curLPslice['spR'][:, ktarget].toarray()
         np.maximum(target_resp, 1e-100, out=target_resp)
-        HrespOrigComp = -1 * NumericUtil.calcRlogR(target_resp)[0]
+        HrespOrigComp = -1 * NumericUtil.calcRlogR(target_resp)
     xSSslice.setELBOTerm('HrespEmptyComp', -1 * HrespOrigComp, dims=None)
 
     # If desired, add merge terms into the expanded summaries,